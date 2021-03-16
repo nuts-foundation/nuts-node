85a57195affcd611b876524c7f06899fbc2b8485
Rein Krul <info@reinkrul.nl>
2021-03-16 20:20:19 +0100

    PR feedback

--- network/dag/bbolt_dag.go
+++ network/dag/bbolt_dag.go
@@ -112,7 +112,6 @@ func (dag *bboltDAG) Diagnostics() []core.DiagnosticResult {
 		return nil
 	})
 	result = append(result, numberOfTransactionsStatistic{numberOfTransactions: transactionNum})
-	// TODO: https://github.com/nuts-foundation/nuts-node/issues/11
 	result = append(result, dataSizeStatistic{sizeInBytes: dag.db.Stats().TxStats.PageAlloc})
 	return result
 }
