079817ef03e7ce00f466523172d2a6925d743228
Wout Slakhorst <wout.slakhorst@nedap.com>
2021-03-25 11:38:44 +0100

    removed irma singleton config

--- auth/services/irma/irmaconfig.go
+++ auth/services/irma/irmaconfig.go
@@ -3,7 +3,6 @@ package irma
 import (
 	"fmt"
 	"os"
-	"sync"
 
 	"github.com/nuts-foundation/nuts-node/auth/logging"
 
@@ -16,62 +15,36 @@ import (
 // IrmaMountPath contains location the irma webserver will mount
 const IrmaMountPath = "/auth/irmaclient"
 
-// create a singleton irma config
-var _irmaConfig *irma.Configuration
-var configOnce = new(sync.Once)
-
-// create a singleton irma server
-var _irmaServer *irmaserver.Server
-var serverOnce = new(sync.Once)
-
 // GetIrmaConfig creates and returns an IRMA config.
 // The config sets the given irma path or a temporary folder. Then it downloads the schemas.
 func GetIrmaConfig(validatorConfig ValidatorConfig) (irmaConfig *irma.Configuration, err error) {
-	irmaConfig = _irmaConfig
+	if err = os.MkdirAll(validatorConfig.IrmaConfigPath, 0700); err != nil {
+		err = errors.Wrap(err, "could not create IRMA config directory")
+		return
+	}
 
-	configOnce.Do(func() {
-		if err = os.MkdirAll(validatorConfig.IrmaConfigPath, 0700); err != nil {
-			err = errors.Wrap(err, "could not create IRMA config directory")
-			return
-		}
+	options := irma.ConfigurationOptions{}
+	irmaConfig, err = irma.NewConfiguration(validatorConfig.IrmaConfigPath, options)
+	if err != nil {
+		return
+	}
 
-		options := irma.ConfigurationOptions{}
-		irmaConfig, err = irma.NewConfiguration(validatorConfig.IrmaConfigPath, options)
-		if err != nil {
-			return
-		}
-
-		logging.Log().Info("Loading IRMA schemas...")
-		err = irmaConfig.ParseFolder()
-		_irmaConfig = irmaConfig
-	})
-	return
+	logging.Log().Info("Loading IRMA schemas...")
+	return irmaConfig, irmaConfig.ParseFolder()
 }
 
 // GetIrmaServer creates and starts the irma server instance.
 // The server can be used by a IRMA client like the app to handle IRMA sessions
-func GetIrmaServer(validatorConfig ValidatorConfig) (irmaServer *irmaserver.Server, err error) {
-	irmaServer = _irmaServer
+func GetIrmaServer(validatorConfig ValidatorConfig, irmaConfig *irma.Configuration) (*irmaserver.Server, error) {
+	config := &server.Configuration{
+		IrmaConfiguration:    irmaConfig,
+		URL:                  fmt.Sprintf("%s"+IrmaMountPath, validatorConfig.PublicURL),
+		Logger:               logging.Log().Logger,
+		SchemesPath:          validatorConfig.IrmaConfigPath,
+		DisableSchemesUpdate: !validatorConfig.AutoUpdateIrmaSchemas,
+	}
 
-	serverOnce.Do(func() {
-		var irmaConfig *irma.Configuration
-		irmaConfig, err = GetIrmaConfig(validatorConfig)
-		if err != nil {
-			return
-		}
-		config := &server.Configuration{
-			IrmaConfiguration:    irmaConfig,
-			URL:                  fmt.Sprintf("%s"+IrmaMountPath, validatorConfig.PublicURL),
-			Logger:               logging.Log().Logger,
-			SchemesPath:          validatorConfig.IrmaConfigPath,
-			DisableSchemesUpdate: !validatorConfig.AutoUpdateIrmaSchemas,
-		}
+	logging.Log().Infof("Initializing IRMA library (baseURL=%s)...", config.URL)
 
-		logging.Log().Infof("Initializing IRMA library (baseURL=%s)...", config.URL)
-
-		irmaServer, err = irmaserver.New(config)
-		_irmaServer = irmaServer
-	})
-
-	return
+	return irmaserver.New(config)
 }
