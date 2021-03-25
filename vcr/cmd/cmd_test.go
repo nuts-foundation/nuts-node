3567362828f2e18ab6c2f5898fc76eb65e4fef42
Wout Slakhorst <wout.slakhorst@nedap.com>
2021-03-25 09:07:07 +0100

    added test for cli

---
+++ vcr/cmd/cmd_test.go
@@ -0,0 +1,155 @@
+/*
+ * Nuts node
+ * Copyright (C) 2021 Nuts community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package cmd
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/nuts-foundation/nuts-node/core"
+	http2 "github.com/nuts-foundation/nuts-node/test/http"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCmd(t *testing.T) {
+	didString := "did:nuts:1"
+	credentialType := "type"
+
+	buf := new(bytes.Buffer)
+
+	newCmd := func(t *testing.T) *cobra.Command {
+		t.Helper()
+		buf.Reset()
+		command := Cmd()
+		command.SetOut(buf)
+		return command
+	}
+
+	cmds := []string{
+		"list-trusted",
+		"list-untrusted",
+	}
+
+	for _, c := range cmds {
+		t.Run(c, func(t *testing.T) {
+			t.Run("ok", func(t *testing.T) {
+				cmd := newCmd(t)
+				s := setupServer(cmd, http.StatusOK, []string{didString})
+				defer reset(s)
+
+				cmd.SetArgs([]string{c, credentialType})
+				err := cmd.Execute()
+
+				if !assert.NoError(t, err) {
+					return
+				}
+				assert.Contains(t, buf.String(), didString)
+			})
+
+			t.Run("error - server error", func(t *testing.T) {
+				cmd := newCmd(t)
+				s := setupServer(cmd, http.StatusInternalServerError, nil)
+				defer reset(s)
+
+				cmd.SetArgs([]string{c, credentialType})
+				err := cmd.Execute()
+
+				if !assert.Error(t, err) {
+					return
+				}
+
+				assert.Contains(t, err.Error(), "VCR returned HTTP 500")
+			})
+
+			t.Run("error - not enough args", func(t *testing.T) {
+				cmd := newCmd(t)
+
+				cmd.SetArgs([]string{c})
+				err := cmd.Execute()
+
+				assert.Error(t, err)
+			})
+		})
+	}
+
+	cmds2 := []string{
+		"trust",
+		"untrust",
+	}
+
+	for _, c := range cmds2 {
+		t.Run(c, func(t *testing.T) {
+			t.Run("ok", func(t *testing.T) {
+				cmd := newCmd(t)
+				s := setupServer(cmd, http.StatusNoContent, nil)
+				defer reset(s)
+
+				cmd.SetArgs([]string{c, credentialType, didString})
+				err := cmd.Execute()
+
+				if !assert.NoError(t, err) {
+					return
+				}
+				assert.Contains(t, buf.String(), didString)
+				assert.Contains(t, buf.String(), credentialType)
+			})
+
+			t.Run("error - server error", func(t *testing.T) {
+				cmd := newCmd(t)
+				s := setupServer(cmd, http.StatusInternalServerError, nil)
+				defer reset(s)
+
+				cmd.SetArgs([]string{c, credentialType, didString})
+				err := cmd.Execute()
+
+				if !assert.Error(t, err) {
+					return
+				}
+
+				assert.Contains(t, err.Error(), "VCR returned HTTP 500")
+			})
+
+			t.Run("error - not enough args", func(t *testing.T) {
+				cmd := newCmd(t)
+
+				cmd.SetArgs([]string{c})
+				err := cmd.Execute()
+
+				assert.Error(t, err)
+			})
+		})
+	}
+}
+
+func setupServer(cmd *cobra.Command, statusCode int, responseData interface{}) *httptest.Server {
+	s := httptest.NewServer(http2.Handler{StatusCode: statusCode, ResponseData: responseData})
+	os.Setenv("NUTS_ADDRESS", s.URL)
+	core.NewClientConfig().Load(cmd.Flags())
+	return s
+}
+
+func reset(httpServer *httptest.Server) {
+	os.Unsetenv("NUTS_ADDRESS")
+	httpServer.Close()
+}
