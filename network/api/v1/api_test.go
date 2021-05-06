2e756ad446572ddd263cc4c660e5f9880d2f0b01
reinkrul <reinkrul@gmail.com>
2021-05-06 09:06:29 +0200

    Network: return JSON problems instead of text/plain errors (#273)

--- network/api/v1/api_test.go
+++ network/api/v1/api_test.go
@@ -18,6 +18,8 @@
 package v1
 
 import (
+	"errors"
+	test2 "github.com/nuts-foundation/nuts-node/test"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -57,6 +59,23 @@ func TestApiWrapper_GetTransaction(t *testing.T) {
 		assert.Equal(t, "application/jose", rec.Header().Get("Content-Type"))
 		assert.Equal(t, string(transaction.Data()), rec.Body.String())
 	})
+	t.Run("error", func(t *testing.T) {
+		var networkClient = network.NewMockTransactions(mockCtrl)
+		e, wrapper := initMockEcho(networkClient)
+		networkClient.EXPECT().GetTransaction(gomock.Any()).Return(nil, errors.New("failed"))
+
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath(path)
+		c.SetParamNames("ref")
+		c.SetParamValues(hash.SHA256Sum([]byte{1, 2, 3}).String())
+
+		err := wrapper.GetTransaction(c)
+		test2.AssertErrProblemTitle(t, problemTitleGetTransaction, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusInternalServerError, err)
+		test2.AssertErrProblemDetail(t, "failed", err)
+	})
 	t.Run("invalid hash", func(t *testing.T) {
 		var networkClient = network.NewMockTransactions(mockCtrl)
 		e, wrapper := initMockEcho(networkClient)
@@ -69,9 +88,9 @@ func TestApiWrapper_GetTransaction(t *testing.T) {
 		c.SetParamValues("1234")
 
 		err := wrapper.GetTransaction(c)
-		assert.NoError(t, err)
-		assert.Equal(t, http.StatusBadRequest, rec.Code)
-		assert.Equal(t, "incorrect hash length (2)", rec.Body.String())
+		test2.AssertErrProblemTitle(t, problemTitleGetTransaction, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusBadRequest, err)
+		test2.AssertErrProblemDetail(t, "incorrect hash length (2)", err)
 	})
 	t.Run("not found", func(t *testing.T) {
 		var networkClient = network.NewMockTransactions(mockCtrl)
@@ -86,9 +105,9 @@ func TestApiWrapper_GetTransaction(t *testing.T) {
 		c.SetParamValues(transaction.Ref().String())
 
 		err := wrapper.GetTransaction(c)
-		assert.NoError(t, err)
-		assert.Equal(t, http.StatusNotFound, rec.Code)
-		assert.Equal(t, "transaction not found", rec.Body.String())
+		test2.AssertErrProblemTitle(t, problemTitleGetTransaction, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusNotFound, err)
+		test2.AssertErrProblemDetail(t, "transaction not found", err)
 	})
 }
 
@@ -111,6 +130,21 @@ func TestApiWrapper_RenderGraph(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, "text/vnd.graphviz", rec.Header().Get("Content-Type"))
 		assert.NotEmpty(t, rec.Body.String())
+	})
+	t.Run("error", func(t *testing.T) {
+		var networkClient = network.NewMockTransactions(mockCtrl)
+		e, wrapper := initMockEcho(networkClient)
+		networkClient.EXPECT().Walk(gomock.Any()).Return(errors.New("failed"))
+
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/graph")
+
+		err := wrapper.RenderGraph(c)
+		test2.AssertErrProblemTitle(t, problemTitleRenderGraph, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusInternalServerError, err)
+		test2.AssertErrProblemDetail(t, "failed", err)
 	})
 }
 
@@ -137,6 +171,23 @@ func TestApiWrapper_GetTransactionPayload(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, string(payload), rec.Body.String())
 	})
+	t.Run("error", func(t *testing.T) {
+		var networkClient = network.NewMockTransactions(mockCtrl)
+		e, wrapper := initMockEcho(networkClient)
+		networkClient.EXPECT().GetTransactionPayload(gomock.Any()).Return(nil, errors.New("failed"))
+
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath(path)
+		c.SetParamNames("ref")
+		c.SetParamValues(transaction.Ref().String())
+
+		err := wrapper.GetTransactionPayload(c)
+		test2.AssertErrProblemTitle(t, problemTitleGetTransactionPayload, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusInternalServerError, err)
+		test2.AssertErrProblemDetail(t, "failed", err)
+	})
 	t.Run("not found", func(t *testing.T) {
 		var networkClient = network.NewMockTransactions(mockCtrl)
 		e, wrapper := initMockEcho(networkClient)
@@ -150,9 +201,9 @@ func TestApiWrapper_GetTransactionPayload(t *testing.T) {
 		c.SetParamValues(transaction.Ref().String())
 
 		err := wrapper.GetTransactionPayload(c)
-		assert.NoError(t, err)
-		assert.Equal(t, http.StatusNotFound, rec.Code)
-		assert.Equal(t, "transaction or contents not found", rec.Body.String())
+		test2.AssertErrProblemTitle(t, problemTitleGetTransactionPayload, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusNotFound, err)
+		test2.AssertErrProblemDetail(t, "transaction or contents not found", err)
 	})
 	t.Run("invalid hash", func(t *testing.T) {
 		var networkClient = network.NewMockTransactions(mockCtrl)
@@ -166,9 +217,9 @@ func TestApiWrapper_GetTransactionPayload(t *testing.T) {
 		c.SetParamValues("1234")
 
 		err := wrapper.GetTransactionPayload(c)
-		assert.NoError(t, err)
-		assert.Equal(t, http.StatusBadRequest, rec.Code)
-		assert.Equal(t, "incorrect hash length (2)", rec.Body.String())
+		test2.AssertErrProblemTitle(t, problemTitleGetTransactionPayload, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusBadRequest, err)
+		test2.AssertErrProblemDetail(t, "incorrect hash length (2)", err)
 	})
 }
 
@@ -177,22 +228,35 @@ func TestApiWrapper_ListTransactions(t *testing.T) {
 	defer mockCtrl.Finish()
 	transaction := dag.CreateTestTransactionWithJWK(1)
 
-	t.Run("list transactions", func(t *testing.T) {
-		t.Run("200", func(t *testing.T) {
-			var networkClient = network.NewMockTransactions(mockCtrl)
-			e, wrapper := initMockEcho(networkClient)
-			networkClient.EXPECT().ListTransactions().Return([]dag.Transaction{transaction}, nil)
+	t.Run("200", func(t *testing.T) {
+		var networkClient = network.NewMockTransactions(mockCtrl)
+		e, wrapper := initMockEcho(networkClient)
+		networkClient.EXPECT().ListTransactions().Return([]dag.Transaction{transaction}, nil)
 
-			req := httptest.NewRequest(echo.GET, "/", nil)
-			rec := httptest.NewRecorder()
-			c := e.NewContext(req, rec)
-			c.SetPath("/transaction")
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/transaction")
 
-			err := wrapper.ListTransactions(c)
-			assert.NoError(t, err)
-			assert.Equal(t, http.StatusOK, rec.Code)
-			assert.Equal(t, `["`+string(transaction.Data())+`"]`, strings.TrimSpace(rec.Body.String()))
-		})
+		err := wrapper.ListTransactions(c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, `["`+string(transaction.Data())+`"]`, strings.TrimSpace(rec.Body.String()))
+	})
+	t.Run("error", func(t *testing.T) {
+		var networkClient = network.NewMockTransactions(mockCtrl)
+		e, wrapper := initMockEcho(networkClient)
+		networkClient.EXPECT().ListTransactions().Return(nil, errors.New("failed"))
+
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		c.SetPath("/transaction")
+
+		err := wrapper.ListTransactions(c)
+		test2.AssertErrProblemTitle(t, problemTitleListTransactions, err)
+		test2.AssertErrProblemStatusCode(t, http.StatusInternalServerError, err)
+		test2.AssertErrProblemDetail(t, "failed", err)
 	})
 }
 
