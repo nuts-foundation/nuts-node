a58e60cd65c516bab380b9a051151da9b7ddb704
Dillen Meijboom <info@excol.nl>
2021-12-23 11:49:42 +0100

    Describe payload type for event subject

--- events/subjects.go
+++ events/subjects.go
@@ -1,4 +1,6 @@
 package events
 
 // PrivateTransactionsSubject defines the NATS subject used for private transactions in the v2 protocol
+//
+// Payload: dag.Transaction
 const PrivateTransactionsSubject = "nuts.v2.private-transactions"
