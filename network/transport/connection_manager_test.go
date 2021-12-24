138695ea6ea8a88e74990b8769b6858d6b02ef6d
Wout Slakhorst <wout.slakhorst@nedap.com>
2021-12-24 11:20:10 +0100

    added test

--- network/transport/connection_manager_test.go
+++ network/transport/connection_manager_test.go
@@ -19,13 +19,22 @@
 package transport
 
 import (
-	"github.com/magiconair/properties/assert"
-	"github.com/nuts-foundation/go-did/did"
 	"testing"
+
+	"github.com/nuts-foundation/go-did/did"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFixedNodeDIDResolver_Resolve(t *testing.T) {
 	expected, _ := did.ParseDID("did:nuts:test")
 	actual, _ := FixedNodeDIDResolver{NodeDID: *expected}.Resolve()
 	assert.Equal(t, actual.String(), expected.String())
+}
+
+func TestWithUnauthenticated(t *testing.T) {
+	peer := Peer{}
+
+	WithUnauthenticated()(&peer)
+
+	assert.True(t, peer.AcceptUnauthenticated)
 }
