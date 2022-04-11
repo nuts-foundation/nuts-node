f5494e49fc668e334ee7db7dfb7a02769b8159f2
Wout Slakhorst <wout.slakhorst@nedap.com>
2022-04-11 15:19:33 +0200

    added JSON-LD type search to VCR

--- vcr/search.go
+++ vcr/search.go
@@ -32,6 +32,7 @@ import (
 )
 
 const (
+<<<<<<< HEAD
 	// Exact is the SearchTerm type that requires a search path to result in an exact match
 	Exact = "exact"
 	// NotNil is the SearchTerm type that requires a search path to result in a non nil value
@@ -41,6 +42,13 @@ const (
 )
 
 // SearchTerm is part of a JSON-LD query. Multiple terms are combined in an 'AND' manner.
+=======
+	Prefix = "prefix"
+	Exact  = "exact"
+	NotNil = "notNil"
+)
+
+>>>>>>> added JSON-LD type search to VCR
 type SearchTerm struct {
 	IRIPath []string
 	Value   interface{}
