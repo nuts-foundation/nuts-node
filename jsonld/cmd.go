7232007a66681c50a5e5e6020854323849a1a522
stevenvegt <steven.vandervegt@nedap.com>
2022-04-14 11:48:21 +0200

    Fix flagset

--- jsonld/cmd.go
+++ jsonld/cmd.go
@@ -7,7 +7,7 @@ import (
 // FlagSet contains flags relevant for VCR
 func FlagSet() *pflag.FlagSet {
 	defs := DefaultConfig()
-	flagSet := pflag.NewFlagSet("vcr", pflag.ContinueOnError)
+	flagSet := pflag.NewFlagSet("jsonld", pflag.ContinueOnError)
 	flagSet.StringSlice("jsonld.contexts.remoteallowlist", defs.Contexts.RemoteAllowList, "In strict mode, fetching external JSON-LD contexts is not allowed except for context-URLs listed here.")
 	flagSet.StringToString("jsonld.contexts.localmapping", defs.Contexts.LocalFileMapping, "This setting allows mapping external URLs to local files for e.g. preventing external dependencies. These mappings have precedence over those in remoteallowlist.")
 	return flagSet
