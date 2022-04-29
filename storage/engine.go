cedc543b9c3077ffe31fe73866e27486302cf13f
Rein Krul <info@reinkrul.nl>
2022-04-29 14:47:12 +0200

    fix

--- storage/engine.go
+++ storage/engine.go
@@ -2,14 +2,15 @@ package storage
 
 import "github.com/nuts-foundation/nuts-node/core"
 
+// New creates a new instance of the storage engine.
 func New() core.Engine {
 	return &engine{}
 }
 
 type engine struct {
-
 }
 
+// Name returns the name of the storage engine.
 func (e engine) Name() string {
 	return "Storage"
 }
