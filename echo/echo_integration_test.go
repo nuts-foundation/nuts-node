07f5f9d5cbd694a0c42a7f8cb3e4ece0ff71198b
Wout Slakhorst <wout.slakhorst@nedap.com>
2022-05-05 09:45:58 +0200

    added API integration test that tests 404s

---
+++ echo/echo_integration_test.go
@@ -0,0 +1,108 @@
+/*
+ * Nuts node
+ * Copyright (C) 2022 Nuts community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package echo
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/nuts-foundation/nuts-node/cmd"
+	"github.com/nuts-foundation/nuts-node/test"
+	"github.com/nuts-foundation/nuts-node/test/io"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestStatusCodes tests if the returned errors from the API implementations are correctly translated to status codes
+func TestStatusCodes(t *testing.T) {
+	httpPort, cancel := startServer(t)
+	defer cancel()
+	defer resetEnv()
+
+	baseUrl := fmt.Sprintf("http://localhost%s", httpPort)
+
+	t.Run("404s", func(t *testing.T) {
+		urls := []string{
+			"/internal/auth/v1/signature/session/1",
+			"/public/auth/v1/contract/1",
+			"/internal/didman/v1/did/did:nuts:1/compoundservice",
+			"/internal/didman/v1/did/did:nuts:1/compoundservice/1/endpoint/2",
+			"/internal/network/v1/transaction/0000000000000000000000000000000000000000000000000000000000000000",
+			"/internal/network/v1/transaction/0000000000000000000000000000000000000000000000000000000000000000/payload",
+			"/internal/vcr/v2/vc/1",
+			"/internal/vdr/v1/did/did:nuts:1",
+		}
+
+		for _, url := range urls {
+			resp, err := http.Get(fmt.Sprintf("%s%s", baseUrl, url))
+
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+		}
+	})
+}
+
+func startServer(t *testing.T) (string, context.CancelFunc) {
+	testDir := io.TestDirectory(t)
+	system := cmd.CreateSystem()
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// command line arguments
+	configFile := path.Join(".", "nuts.yaml")
+	grpcPort := fmt.Sprintf(":%d", test.FreeTCPPort())
+	natsPort := fmt.Sprintf("%d", test.FreeTCPPort())
+	httpPort := fmt.Sprintf(":%d", test.FreeTCPPort())
+
+	os.Setenv("NUTS_DATADIR", testDir)
+	os.Setenv("NUTS_CONFIGFILE", configFile)
+	os.Setenv("NUTS_HTTP_DEFAULT_ADDRESS", httpPort)
+	os.Setenv("NUTS_NETWORK_GRPCADDR", grpcPort)
+	os.Setenv("NUTS_EVENTS_NATS_PORT", natsPort)
+	os.Args = []string{"nuts", "server"}
+
+	go func() {
+		err := cmd.Execute(ctx, system)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	test.WaitFor(t, func() (bool, error) {
+		resp, err := http.Get(fmt.Sprintf("http://localhost%s/status", httpPort))
+		return err == nil && resp.StatusCode == http.StatusOK, nil
+	}, time.Second, "Timeout while waiting for node to become available")
+
+	return httpPort, cancel
+}
+
+func resetEnv() {
+	os.Unsetenv("NUTS_CONFIGFILE")
+	os.Unsetenv("NUTS_DATADIR")
+	os.Unsetenv("NUTS_HTTP_DEFAULT_ADDRESS")
+	os.Unsetenv("NUTS_NETWORK_GRPCADDR")
+	os.Unsetenv("NUTS_EVENTS_NATS_PORT")
+}
