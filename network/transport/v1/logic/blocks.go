6f91df001c8ac1fe32f2897237583932ce602f24
reinkrul <reinkrul@gmail.com>
2022-05-12 10:53:57 +0200

    Revert "remove V1 network protocol and unused code (#1061)"
    This reverts commit 78d0e466affa29422bca9d0c6f6919e6fb966214.

---
+++ network/transport/v1/logic/blocks.go
@@ -0,0 +1,298 @@
+/*
+ * Copyright (C) 2021 Nuts community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package logic
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/nuts-foundation/nuts-node/crypto/hash"
+	"github.com/nuts-foundation/nuts-node/network/dag"
+)
+
+const numberOfBlocks = 3
+
+// trackingDAGBlocks is a dagBlocks implementation tracks the DAG block heads in a memory friendly way.
+// It works by storing the known heads of a block and the distance (in blocks) to the next TX (by which it is prev'd),
+// so it can be unmarked as head when the next TX is moved into the historic block.
+// When midnight passes (and thus blocks change) it shifts all TXs one block to the left, ultimately into the leftmost historic block.
+type trackingDAGBlocks struct {
+	blocks []*blockTracker
+	// processedTransactions keeps track of added transaction to prevent duplicates doing harm.
+	// This is an ever growing list and thus requires some refactoring in the future.
+	processedTransactions map[hash.SHA256Hash]bool
+	mux                   *sync.Mutex
+}
+
+// dagBlocks defines the API for algorithms that determine the head transactions for DAG blocks.
+type dagBlocks interface {
+	// String returns the state of the algorithm as string.
+	String() string
+	// get returns a slice containing the DAG blocks left-to-right (historic block at [0], current block at [len(blocks) - 1]).
+	get() []dagBlock
+	// addTransaction adds a transaction to the DAG blocks structure. It MUST be called in actual transactions order,
+	// So given TXs `A <- B <- [C, D]` call order is A, B, C, D (or A, B, D, C).
+	// It will typically be called using a sequential DAG subscriber.
+	addTransaction(tx dag.Transaction, _ []byte) error
+}
+
+// newDAGBlocks creates a new tracking dagBlocks structure.
+func newDAGBlocks() dagBlocks {
+	result := &trackingDAGBlocks{
+		blocks:                make([]*blockTracker, numberOfBlocks),
+		processedTransactions: map[hash.SHA256Hash]bool{},
+		mux:                   &sync.Mutex{},
+	}
+	for i := 0; i < len(result.blocks); i++ {
+		result.blocks[i] = &blockTracker{heads: map[hash.SHA256Hash]*head{}}
+	}
+	result.internalUpdateTimestamps(time.Now())
+	return result
+}
+
+// dagBlock is a DAG block.
+type dagBlock struct {
+	// start contains the start time of the block.
+	start time.Time
+	// heads contains the heads of the block.
+	heads []hash.SHA256Hash
+}
+
+// xor calculates a hash over all heads using bitwise xor.
+func (b dagBlock) xor() hash.SHA256Hash {
+	var result hash.SHA256Hash
+	multiXOR(&result, b.heads...)
+	return result
+}
+
+type blockTracker struct {
+	start time.Time
+	// key: tx ref, value: distance (in blocks) of the next TX
+	heads map[hash.SHA256Hash]*head
+}
+
+type head struct {
+	// distance contains number of blocks to the next TX that refers to this TX as 'prev'. If the next TX is in the next
+	// block, distance will be 1. If `distance is 0, it means the next TX is in the same block. It is initialized
+	// to math.MaxInt32, meaning no next TX.
+	distance    int32
+	signingTime time.Time
+	// blockDate contains the signing time at start of the block
+	blockDate time.Time
+}
+
+// internalGet calculates the block heads, without updating the structure first. Only for internal use and testing.
+func (blx *trackingDAGBlocks) internalGet() []dagBlock {
+	result := make([]dagBlock, 0)
+	for blockNum, currBlock := range blx.blocks {
+		resultBlock := dagBlock{start: currBlock.start}
+		for ref := range currBlock.heads {
+			if blockNum < len(blx.blocks) {
+				resultBlock.heads = append(resultBlock.heads, ref)
+			}
+		}
+		result = append(result, resultBlock)
+	}
+	return result
+}
+
+// get returns the current block heads. Successive calls might return a different result since they're distributed in relation to the current day.
+func (blx *trackingDAGBlocks) get() []dagBlock {
+	blx.mux.Lock()
+	defer blx.mux.Unlock()
+	blx.internalUpdate(time.Now())
+	return blx.internalGet()
+}
+
+// AddTransaction adds a transaction to the DAG blocks structure. It MUST be called for transactions in order,
+// so it's typically called using a sequential DAG subscriber.
+// So given TXs `A <- B <- [C, D]` call order is A, B, C, D (or A, B, D, C).
+func (blx *trackingDAGBlocks) addTransaction(tx dag.Transaction, _ []byte) error {
+	blx.mux.Lock()
+	defer blx.mux.Unlock()
+
+	if processed := blx.processedTransactions[tx.Ref()]; processed {
+		return nil
+	}
+
+	blx.internalUpdate(time.Now())
+	blx.internalAddTransaction(tx)
+
+	blx.processedTransactions[tx.Ref()] = true
+
+	return nil
+}
+
+func (blx *trackingDAGBlocks) internalAddTransaction(tx dag.Transaction) {
+	// Determine block the TX is part of
+	blockIdx := blx.internalGetBlockIndex(tx.SigningTime())
+	txBlock := blx.blocks[blockIdx]
+	// Prevs of this TX in this block were previously heads (prev'd by another branch within this block) but now
+	// the current TX will be the new head. So we 'un-head' all prevs and mark the current TX as head.
+	// This works as long as this func is called with TXs in order.
+	txBlockDate := startOfDay(tx.SigningTime())
+	for _, prev := range tx.Previous() {
+		if _, ok := txBlock.heads[prev]; ok {
+			delete(txBlock.heads, prev)
+		}
+	}
+	txBlock.heads[tx.Ref()] = &head{
+		distance:    math.MaxInt32,
+		signingTime: tx.SigningTime(),
+		blockDate:   txBlockDate,
+	}
+	// Find prevs is this TX that are currently heads in the previous blocks and set the shortest distance.
+	// When the blocks are updated and the "next TX's" block distance reaches zero,
+	// that means the next TX is in the same block and thus the head isn't a head any more.
+	for i := 0; i <= blockIdx; i++ {
+		for _, currPrev := range tx.Previous() {
+			if head, ok := blx.blocks[i].heads[currPrev]; ok {
+				newDistance := int32(blockIdx - blx.internalGetBlockIndex(head.signingTime))
+				if newDistance < head.distance {
+					head.distance = newDistance
+				}
+			}
+		}
+	}
+}
+
+func (blx *trackingDAGBlocks) internalGetBlockIndex(txSigningTime time.Time) int {
+	blockIdx := len(blx.blocks) - 1
+	for i := 0; i < len(blx.blocks)-1; i++ {
+		// Lower blocks are earlier, so when the TXs time is before the start time of the next block, this is the block
+		// the TX is part of.
+		if txSigningTime.Before(blx.blocks[i+1].start) {
+			blockIdx = i
+			break
+		}
+	}
+	return blockIdx
+}
+
+// internalUpdate first updates the timestamps on the blocks and then redistributes the transactions into the correct blocks.
+// It must be called before any interaction (reading the heads, adding a transaction) with the structure.
+// Callers must make sure it's never called with an older timestamp (a timestamp which lies before the timestamp it was last called with).
+func (blx *trackingDAGBlocks) internalUpdate(now time.Time) {
+	if !blx.internalUpdateTimestamps(now) {
+		// Day didn't pass since last call so block timestamps are not updated -> nothing to do.
+		return
+	}
+	// Block timestamps were updated, now move the TXs to their new blocks. This will generally mean shift 1 block to the left,
+	// except for TXs with are already in the leftmost block and TXs that are in the current block and remain there
+	// (because the signing time lies in the future).
+	numBlocks := len(blx.blocks)
+	for i := 0; i < numBlocks; i++ {
+		curr := blx.blocks[i]
+		if i == 0 {
+			updateTXBlockDistances(curr)
+		}
+		if i < numBlocks-1 {
+			next := blx.blocks[i+1]
+			blx.leftShiftTXs(curr, next)
+		}
+	}
+}
+
+// leftShiftTXs shifts the transactions in the right block to the left block,
+// but only if the signing time puts it into the left block (which can be the case for TXs in current day's block).
+func (blx *trackingDAGBlocks) leftShiftTXs(left *blockTracker, right *blockTracker) {
+	for ref, head := range right.heads {
+		if head.signingTime.Before(right.start) {
+			left.heads[ref] = head
+			delete(right.heads, ref)
+		}
+	}
+}
+
+// updateTXBlockDistances updates the historic block:
+// - decrement `distance`
+// - when `distance` reaches zero, the unmark the TX as block head
+func updateTXBlockDistances(historicBlock *blockTracker) {
+	for ref, head := range historicBlock.heads {
+		if head.distance != math.MaxInt32 {
+			head.distance--
+			if head.distance == 0 {
+				// Next TX of the head TX now falls within this block, so unmark it as block head
+				delete(historicBlock.heads, ref)
+			}
+		}
+	}
+}
+
+// internalUpdateTimestamps updates the timestamps of the blocks using the given time.
+// If there's already transactions in the blocks they must be redistributed (so they end up in the correct block) after this function has been called.
+func (blx *trackingDAGBlocks) internalUpdateTimestamps(now time.Time) bool {
+	t := startOfDay(now)
+	changed := false
+	for idx, currBlock := range blx.blocks {
+		// Start time of first block (historic block) is beginning of time, so it doesn't need to be updated
+		if idx == 0 {
+			continue
+		}
+		newDate := t.AddDate(0, 0, idx-len(blx.blocks)+1)
+		if !newDate.Equal(currBlock.start) {
+			currBlock.start = newDate
+			changed = true
+		}
+	}
+	return changed
+}
+
+func (blx trackingDAGBlocks) String() string {
+	lines := make([]string, len(blx.blocks))
+	for i := 0; i < len(lines); i++ {
+		lines[i] = fmt.Sprintf("  [%d] %v", i, *blx.blocks[i])
+	}
+	return fmt.Sprintf("blocks:\n%s", strings.Join(lines, "\n"))
+}
+
+func (b blockTracker) String() string {
+	if len(b.heads) > 0 {
+		items := make([]string, 0, len(b.heads))
+		for ref, head := range b.heads {
+			items = append(items, fmt.Sprintf("%s:%d", ref, head.distance))
+		}
+		return strings.Join(items, ", ")
+	}
+	return "(empty)"
+}
+
+func startOfDay(now time.Time) time.Time {
+	nowUTC := now.In(time.UTC)
+	return time.Date(nowUTC.Year(), nowUTC.Month(), nowUTC.Day(), 0, 0, 0, 0, time.UTC)
+}
+
+func xor(dest *hash.SHA256Hash, left, right hash.SHA256Hash) {
+	for i := 0; i < len(left); i++ {
+		dest[i] = left[i] ^ right[i]
+	}
+}
+
+func multiXOR(dest *hash.SHA256Hash, hashes ...hash.SHA256Hash) {
+	if len(hashes) == 0 {
+		return
+	}
+	*dest = hashes[0]
+	for i := 1; i < len(hashes); i++ {
+		xor(dest, *dest, hashes[i])
+	}
+}
---
+++ network/transport/v1/logic/handlers.go
@@ -0,0 +1,324 @@
+/*
+ * Copyright (C) 2021 Nuts community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package logic
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/nuts-foundation/nuts-node/crypto/hash"
+	"github.com/nuts-foundation/nuts-node/network/dag"
+	"github.com/nuts-foundation/nuts-node/network/log"
+	"github.com/nuts-foundation/nuts-node/network/transport"
+	"github.com/nuts-foundation/nuts-node/network/transport/v1/protobuf"
+	"github.com/sirupsen/logrus"
+)
+
+func (p *protocol) Handle(peer transport.Peer, raw interface{}) error {
+	networkMessage := raw.(*protobuf.NetworkMessage)
+	peerID := peer.ID
+
+	switch msg := networkMessage.Message.(type) {
+	case *protobuf.NetworkMessage_AdvertHashes:
+		if msg.AdvertHashes != nil {
+			p.handleAdvertHashes(peerID, msg.AdvertHashes)
+		}
+	case *protobuf.NetworkMessage_TransactionListQuery:
+		if msg.TransactionListQuery != nil {
+			return p.handleTransactionListQuery(peerID, msg.TransactionListQuery.BlockDate)
+		}
+	case *protobuf.NetworkMessage_TransactionList:
+		if msg.TransactionList != nil {
+			return p.handleTransactionList(peerID, msg.TransactionList)
+		}
+	case *protobuf.NetworkMessage_TransactionPayloadQuery:
+		if msg.TransactionPayloadQuery != nil && msg.TransactionPayloadQuery.PayloadHash != nil {
+			return p.handleTransactionPayloadQuery(peerID, msg.TransactionPayloadQuery)
+		}
+	case *protobuf.NetworkMessage_TransactionPayload:
+		if msg.TransactionPayload != nil && msg.TransactionPayload.PayloadHash != nil && msg.TransactionPayload.Data != nil {
+			p.handleTransactionPayload(peerID, msg.TransactionPayload)
+		}
+	case *protobuf.NetworkMessage_DiagnosticsBroadcast:
+		p.handleDiagnostics(peerID, msg.DiagnosticsBroadcast)
+	default:
+		return errors.New("envelope doesn't contain any (handleable) messages")
+	}
+	return nil
+}
+
+func (p *protocol) handleAdvertHashes(peer transport.PeerID, advertHash *protobuf.AdvertHashes) {
+	log.Logger().Tracef("Received adverted hashes from peer: %s", peer)
+
+	localBlocks := p.blocks.get()
+	// We could theoretically support clock skew of peers (compared to the local clock), but we can also just wait for
+	// the blocks to synchronize which is way easier. It could just lead to a little synchronization delay at midnight.
+	localCurrentBlockDate := getBlockTimestamp(getCurrentBlock(localBlocks).start)
+	if localCurrentBlockDate != advertHash.CurrentBlockDate {
+		// Logger level is INFO which might prove to be too verbose, but we'll have to find out in an actual network.
+		log.Logger().Infof("Peer's current block date differs (probably due to clock skew) which is not supported, broadcast is ignored (peer=%s,local blockdate=%d,peer blockdate=%d)", peer, localCurrentBlockDate, advertHash.CurrentBlockDate)
+		return
+	}
+	// Block count is spec'd, so they must not differ.
+	if len(localBlocks)-1 != len(advertHash.Blocks) {
+		log.Logger().Warnf("Peer's number of block differs which is not supported, broadcast is ignored (peer=%s)", peer)
+		return
+	}
+	// Compare historic block
+	localHistoricBlock := getHistoricBlock(localBlocks)
+	localHistoryHash := localHistoricBlock.xor()
+	peerHistoryHash := hash.FromSlice(advertHash.HistoricHash)
+	if !localHistoryHash.Equals(peerHistoryHash) {
+		// TODO: Disallowed when https://github.com/nuts-foundation/nuts-specification/issues/57 is implemented
+		log.Logger().Debugf("Peer's historic block differs which will be sync-ed for now, but will be disallowed in the future (peer=%s,local hash=%s,peer hash=%s)", peer, localHistoryHash, peerHistoryHash)
+		p.sender.sendTransactionListQuery(peer, time.Time{})
+	} else {
+		// Finally, check the rest of the blocks
+		p.checkPeerBlocks(peer, advertHash.Blocks, localBlocks[1:])
+	}
+
+	// Calculate peer's omnihash and propagate it for diagnostic purposes.
+	omnihash := peerHistoryHash
+	for _, blck := range advertHash.Blocks {
+		for _, head := range blck.Hashes {
+			xor(&omnihash, omnihash, hash.FromSlice(head))
+		}
+	}
+	p.peerOmnihashChannel <- PeerOmnihash{
+		Peer: peer,
+		Hash: omnihash,
+	}
+}
+
+// checkPeerBlocks compares the blocks we've received from a peer with the ones of the local DAG. If it finds heads in the
+// peer blocks that aren't present on the local DAG is will query that block's transactions.
+// localBlocks must not include the historic block.
+func (p *protocol) checkPeerBlocks(peer transport.PeerID, peerBlocks []*protobuf.BlockHashes, localBlocks []dagBlock) {
+	for i := 0; i < len(peerBlocks); i++ {
+		localBlock := localBlocks[i]
+		peerBlock := peerBlocks[i]
+		for _, peerHead := range peerBlock.Hashes {
+			peerHeadHash := hash.FromSlice(peerHead)
+			headMatches := false
+			for _, localHead := range localBlock.heads {
+				if peerHeadHash.Equals(localHead) {
+					headMatches = true
+					break
+				}
+			}
+			if headMatches {
+				continue
+			}
+			// peerHead is not one of our heads in the same block, which either means:
+			// - Peer has a TX we don't have
+			// - We have a TX our peer doesn't have
+			// To find out which is the case we check whether we have the peer's head as TX on our DAG.
+			// If not, we're missing the peer's TX on our DAG and should query the block.
+			headIsPresentOnLocalDAG, err := p.state.IsPresent(context.Background(), peerHeadHash)
+			if err != nil {
+				log.Logger().Errorf("Error while checking peer head on local DAG (ref=%s): %v", peerHeadHash, err)
+			} else if !headIsPresentOnLocalDAG {
+				log.Logger().Infof("Peer has head which is not present on our DAG, querying block's transactions (peer=%s, tx=%s, blockDate=%s)", peer, peerHeadHash, localBlock.start)
+				p.sender.sendTransactionListQuery(peer, localBlock.start)
+			}
+		}
+	}
+}
+
+func (p *protocol) handleTransactionPayload(peer transport.PeerID, contents *protobuf.TransactionPayload) {
+	payloadHash := hash.FromSlice(contents.PayloadHash)
+	ctx := context.Background()
+	log.Logger().Infof("Received transaction payload from peer (peer=%s,payloadHash=%s,len=%d)", peer, payloadHash, len(contents.Data))
+	if transaction, err := p.state.GetByPayloadHash(ctx, payloadHash); err != nil {
+		log.Logger().Errorf("Error while looking up transaction to write payload (payloadHash=%s): %v", payloadHash, err)
+	} else if len(transaction) == 0 {
+		// This might mean an attacker is sending us unsolicited document payloads
+		log.Logger().Infof("Received transaction payload for transaction we don't have (payloadHash=%s)", payloadHash)
+	} else if err := p.state.WritePayload(ctx, nil, payloadHash, contents.Data); err != nil {
+		log.Logger().Errorf("Error while writing payload for transaction (hash=%s): %v", payloadHash, err)
+	}
+}
+
+func (p *protocol) handleTransactionPayloadQuery(peer transport.PeerID, query *protobuf.TransactionPayloadQuery) error {
+	payloadHash := hash.FromSlice(query.PayloadHash)
+	log.Logger().Tracef("Received transaction payload query from peer (peer=%s, payloadHash=%s)", peer, payloadHash)
+
+	ctx := context.Background()
+
+	transactions, err := p.state.GetByPayloadHash(ctx, payloadHash)
+	if err != nil {
+		return err
+	}
+
+	// We need to return an empty payload for transactions with a to address in v1 protocol
+	for _, tx := range transactions {
+		if len(tx.PAL()) > 0 {
+			p.sender.sendTransactionPayload(peer, payloadHash, []byte{})
+
+			return nil
+		}
+	}
+
+	data, err := p.state.ReadPayload(ctx, payloadHash)
+	if err != nil {
+		return err
+	}
+
+	if data == nil {
+		log.Logger().Debugf("Peer queried us for transaction payload, but seems like we don't have it (peer=%s,payloadHash=%s)", peer, payloadHash)
+	}
+
+	p.sender.sendTransactionPayload(peer, payloadHash, data)
+
+	return nil
+}
+
+func (p *protocol) handleTransactionList(peer transport.PeerID, transactionList *protobuf.TransactionList) error {
+	// TODO: Only process transaction list if we actually queried it (but be aware of pagination)
+	// TODO: Do something with blockDate
+	log.Logger().Tracef("Received transaction list from peer (peer=%s)", peer)
+	ctx := context.Background()
+	transactions := transactionList.Transactions
+	transactionProcessed := true
+	for transactionProcessed {
+		transactionProcessed = false
+		for i := 0; i < len(transactions); i++ {
+			current := transactions[i]
+			transactionRef := hash.FromSlice(current.Hash)
+			if !transactionRef.Equals(hash.SHA256Sum(current.Data)) {
+				return errors.New("received transaction hash doesn't match transaction bytes")
+			}
+			err := p.checkTransactionOnLocalNode(ctx, peer, transactionRef, current.Data)
+			// No error = OK, added to DAG. Remove from list
+			if err == nil {
+				transactionProcessed = true
+				transactions = append(transactions[:i], transactions[i+1:]...)
+				i--
+				continue
+			}
+			// If transaction is missing a previous transaction it should be retried later since transactions
+			// might have been received out of order. In any other case the response is invalid, so we return.
+			if !errors.Is(err, dag.ErrPreviousTransactionMissing) {
+				return err
+			}
+		}
+	}
+	// There may be transactions left that couldn't be processed
+	for _, tx := range transactions {
+		log.Logger().Warnf("Received unprocessable transaction because previous transactions are missing (peer=%s,tx=%s)", peer, hash.FromSlice(tx.Hash))
+	}
+	return nil
+}
+
+// checkTransactionOnLocalNode checks whether the given transaction is present on the local node, adds it if not and/or queries
+// the payload if it (the payload) it not present. If we have both transaction and payload, nothing is done.
+func (p *protocol) checkTransactionOnLocalNode(ctx context.Context, peer transport.PeerID, transactionRef hash.SHA256Hash, data []byte) error {
+	// TODO: Make this a bit smarter.
+	var transaction dag.Transaction
+	var err error
+
+	if transaction, err = dag.ParseTransaction(data); err != nil {
+		return fmt.Errorf("received transaction is invalid (peer=%s,pref=%s): %w", peer, transactionRef, err)
+	}
+
+	queryContents := true
+	if err := p.state.Add(ctx, transaction, nil); err != nil {
+		return fmt.Errorf("unable to add received transaction to DAG (tx=%s): %w", transaction.Ref(), err)
+	}
+	if payloadPresent, err := p.state.IsPayloadPresent(ctx, transaction.PayloadHash()); err != nil {
+		return err
+	} else {
+		queryContents = !payloadPresent
+	}
+
+	if queryContents {
+		// If the transaction contains a to address, we need to ignore it as it should be handled by the v2 protocol
+		if len(transaction.PAL()) > 0 {
+			return nil
+		}
+
+		// TODO: Currently we send the query to the peer that sent us the hash, but this peer might not have the
+		//   transaction contents. We need a smarter way to get it from a peer who does.
+		log.Logger().Infof("Received transaction hash from peer that we don't have yet or we're missing its contents, will query it (peer=%s,hash=%s)", peer, transactionRef)
+		p.sender.sendTransactionPayloadQuery(peer, transaction.PayloadHash())
+	}
+
+	return nil
+}
+
+func (p *protocol) handleTransactionListQuery(peer transport.PeerID, blockDateInt uint32) error {
+	var startDate time.Time
+	var endDate time.Time
+	if blockDateInt == 0 {
+		// TODO: Disallowed when https://github.com/nuts-foundation/nuts-specification/issues/57 is implemented
+		logrus.Debugf("Peer queries historic block which is supported for now, but will be disallowed in the future (peer=%s)", peer)
+		// Historic block is queried, query from start up to the first block
+		endDate = p.blocks.get()[1].start
+	} else {
+		startDate = time.Unix(int64(blockDateInt), 0)
+		endDate = startDate.AddDate(0, 0, 1)
+	}
+	log.Logger().Tracef("Received transaction list query from peer (peer=%s,from=%s,to=%s)", peer, startDate, endDate)
+	txs, err := p.state.FindBetween(context.Background(), startDate, endDate)
+	if err != nil {
+		return err
+	}
+	p.sender.sendTransactionList(peer, txs, startDate)
+	return nil
+}
+
+func (p *protocol) handleDiagnostics(peer transport.PeerID, response *protobuf.Diagnostics) {
+	diagnostics := transport.Diagnostics{
+		Uptime:               time.Duration(response.Uptime) * time.Second,
+		NumberOfTransactions: response.NumberOfTransactions,
+		SoftwareVersion:      response.SoftwareVersion,
+		SoftwareID:           response.SoftwareID,
+	}
+	for _, peer := range response.Peers {
+		diagnostics.Peers = append(diagnostics.Peers, transport.PeerID(peer))
+	}
+	withLock(p.peerDiagnosticsMutex, func() {
+		p.peerDiagnostics[peer] = diagnostics
+	})
+}
+
+func toNetworkTransactions(transactions []dag.Transaction) []*protobuf.Transaction {
+	result := make([]*protobuf.Transaction, len(transactions))
+	for i, transaction := range transactions {
+		result[i] = &protobuf.Transaction{
+			Hash: transaction.Ref().Slice(),
+			Data: transaction.Data(),
+		}
+	}
+	return result
+}
+
+func getBlockTimestamp(timestamp time.Time) uint32 {
+	return uint32(timestamp.Unix())
+}
+
+func getCurrentBlock(blocks []dagBlock) dagBlock {
+	return blocks[len(blocks)-1]
+}
+
+func getHistoricBlock(blocks []dagBlock) dagBlock {
+	return blocks[0]
+}
