5d6720251d7f182787a4fdcd86521460e99e0cac
Rein Krul <info@reinkrul.nl>
2022-08-02 10:02:11 +0200

    test

--- core/echo_test.go
+++ core/echo_test.go
@@ -196,11 +196,13 @@ func Test_createEchoServer(t *testing.T) {
 
 }
 
-func Test_requestsStatusEndpoint(t *testing.T) {
+func Test_skipLogRequest(t *testing.T) {
 	req := &http.Request{}
 	ctx := echo.New().NewContext(req, nil)
 	t.Run("matches", func(t *testing.T) {
 		req.RequestURI = "/status"
+		assert.True(t, skipLogRequest(ctx))
+		req.RequestURI = "/metrics"
 		assert.True(t, skipLogRequest(ctx))
 	})
 	t.Run("no match", func(t *testing.T) {
