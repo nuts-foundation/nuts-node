3b1027581cde44c1e4bc9dd94ab8a2c30fda1a37
Rein Krul <info@reinkrul.nl>
2022-08-23 11:25:49 +0200

    codeclimate

--- core/http_client.go
+++ core/http_client.go
@@ -57,7 +57,7 @@ func (w httpRequestDoerAdapter) Do(req *http.Request) (*http.Response, error) {
 // This does not use the generated client options for e.g. authentication,
 // because each generated OpenAPI client reimplements the client options using structs,
 // which makes them incompatible with each other, making it impossible to use write generic client code for common traits like authorization.
-func CreateHTTPClient(cfg ClientConfig) (*httpRequestDoerAdapter, error) {
+func CreateHTTPClient(cfg ClientConfig) (HTTPRequestDoer, error) {
 	var result *httpRequestDoerAdapter
 	client := &http.Client{}
 	client.Timeout = cfg.Timeout
