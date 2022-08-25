5b4a4929aa6be5ea589225b8e2f682c52bc0baee
Rein Krul <info@reinkrul.nl>
2022-08-25 11:51:59 +0200

    codeclimate

--- storage/redis.go
+++ storage/redis.go
@@ -54,6 +54,7 @@ func (r RedisConfig) IsConfigured() bool {
 	return len(r.Address) > 0
 }
 
+// RedisTLSConfig species properties for connecting to a Redis server over TLS.
 type RedisTLSConfig struct {
 	TrustStoreFile string `koanf:"truststorefile"`
 }
