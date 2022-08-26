d36837bae48b780bfb76134e85b506472fc207a5
reinkrul <info@reinkrul.nl>
2022-08-26 11:11:06 +0200

    Security: validate key IDs before passing them to storage (#1380)

--- crypto/decrypter_test.go
+++ crypto/decrypter_test.go
@@ -25,16 +25,25 @@ import (
 )
 
 func TestCrypto_Decrypt(t *testing.T) {
-	client := createCrypto(t)
-	kid := "kid"
-	key, _ := client.New(StringNamingFunc(kid))
-	pubKey := key.Public().(*ecdsa.PublicKey)
+	t.Run("ok", func(t *testing.T) {
+		client := createCrypto(t)
+		kid := "kid"
+		key, _ := client.New(StringNamingFunc(kid))
+		pubKey := key.Public().(*ecdsa.PublicKey)
 
-	cipherText, err := EciesEncrypt(pubKey, []byte("hello!"))
-	assert.NoError(t, err)
+		cipherText, err := EciesEncrypt(pubKey, []byte("hello!"))
+		assert.NoError(t, err)
 
-	plainText, err := client.Decrypt("kid", cipherText)
-	assert.NoError(t, err)
+		plainText, err := client.Decrypt("kid", cipherText)
+		assert.NoError(t, err)
 
-	assert.Equal(t, "hello!", string(plainText))
+		assert.Equal(t, "hello!", string(plainText))
+	})
+	t.Run("error - invalid kid", func(t *testing.T) {
+		client := createCrypto(t)
+
+		_, err := client.Decrypt("../ceritifcate", nil)
+
+		assert.ErrorContains(t, err, "invalid key ID")
+	})
 }
