b3a90a430cc36b5d4730869b379c987cee1cb5d0
Wout Slakhorst <wout.slakhorst@nedap.com>
2022-08-31 09:46:15 +0200

    Network: verify new transactions outside of storage write-transaction (#1393)
    Co-authored-by: Rein Krul <info@reinkrul.nl>

--- network/dag/state.go
+++ network/dag/state.go
@@ -148,17 +148,37 @@ func (s *state) Add(ctx context.Context, transaction Transaction, payload []byte
 	txAdded := false
 	emitPayloadEvent := false
 
-	return s.db.Write(ctx, func(tx stoabs.WriteTx) error {
-		present := s.graph.isPresent(tx, transaction.Ref())
+	// the tx may contain a large number of prevs. Reading those TXs inside the write-transaction may cause it to timeout.
+	// See https://github.com/nuts-foundation/nuts-node/issues/1391
+	var present bool
+	if err := s.db.Read(ctx, func(tx stoabs.ReadTx) error {
+		// Check TX presence before calling verifiers to avoid executing expensive checks (e.g. TXs with lots of prevs, signatures)
+		// It does not prevent 100% of duplicate checks since race conditions may apply during a read TX.
+		present = s.graph.isPresent(tx, transaction.Ref())
 		if present {
 			return nil
 		}
+		return s.verifyTX(tx, transaction)
+	}); err != nil {
+		return err
+	}
+	if present {
+		// TX already present on DAG, nothing to do
+		return nil
+	}
+
+	return s.db.Write(ctx, func(tx stoabs.WriteTx) error {
+		// TX already present on DAG, nothing to do
+		// We need to do this check again, because a concurrent call could've added the TX (e.g. we got it from another peer).
+		// This is due to verifications being performed in a separate read-transaction above.
+		// A TX must not be added twice, because it will corrupt the XOR and IBLT trees.
+		if s.graph.isPresent(tx, transaction.Ref()) {
+			return nil
+		}
+
 		// control the afterCommit hooks
 		txAdded = true
 
-		if err := s.verifyTX(tx, transaction); err != nil {
-			return err
-		}
 		if payload != nil {
 			emitPayloadEvent = true
 			payloadHash := hash.SHA256Sum(payload)
