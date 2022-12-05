cfeb57812eebf2ecdd8bfe2d3280dc5dab54dba5
Wout Slakhorst <wout.slakhorst@nedap.com>
2022-12-05 11:57:23 +0100

    fix interface comment

--- network/interface.go
+++ network/interface.go
@@ -50,7 +50,6 @@ type Transactions interface {
 	// PeerDiagnostics returns a map containing diagnostic information of the node's peers. The key contains the remote peer's ID.
 	PeerDiagnostics() map[transport.PeerID]transport.Diagnostics
 	// Reprocess walks the DAG and publishes all transactions matching the contentType via Nats
-	// This is an async process and will not return any feedback
 	Reprocess(ctx context.Context, contentType string) (*ReprocessReport, error)
 	// WithPersistency returns a SubscriberOption for persistency. It allows the DAG KVStore to be used as persistent store for notifications.
 	// The notifications will then have ACID properties
