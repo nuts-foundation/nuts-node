3674959a69fc7a124422afbb2b661e00dca8acad
beardedfoo <773275+beardedfoo@users.noreply.github.com>
2022-12-13 08:38:40 +0000

    move require before .Regexp()

--- crypto/storage/vault_test.go
+++ crypto/storage/vault_test.go
@@ -223,9 +223,8 @@ func TestNewVaultKVStorage(t *testing.T) {
 
 	t.Run("error - wrong URL", func(t *testing.T) {
 		storage, err := NewVaultKVStorage(VaultConfig{Address: "http://non-existing"})
-		assert.Error(t, err)
-		assert.Regexp(t, `no such host|Temporary failure in name resolution`, err.Error())
 		require.Error(t, err)
+		assert.Regexp(t, `no such host|Temporary failure in name resolution`, err.Error())
 		assert.Nil(t, storage)
 	})
 }
