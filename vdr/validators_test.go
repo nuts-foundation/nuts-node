be9ff025256b170bb040f85f64faa88294cda533
Gerard Snaauw <gerard.snaauw@nedap.com>
2022-12-19 15:24:38 +0100

    VDR: Add ManagedDocumentValidator and managedServiceValidator (#1769)
    * add managedServiceValidator
    
    * comment on conflicted service resolution
    
    * fix case where service references same did document

--- vdr/validators_test.go
+++ vdr/validators_test.go
@@ -23,133 +23,261 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"errors"
+	"github.com/golang/mock/gomock"
 	ssi "github.com/nuts-foundation/go-did"
 	"github.com/nuts-foundation/go-did/did"
+	"github.com/nuts-foundation/nuts-node/vdr/didservice"
+	"github.com/nuts-foundation/nuts-node/vdr/types"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_verificationMethodValidator(t *testing.T) {
-	type args struct {
-		doc did.Document
-	}
-	tests := []struct {
-		name      string
-		beforeFn  func(t *testing.T, a *args)
-		wantedErr error
-	}{
-		{"ok - valid document", func(t *testing.T, a *args) {
+	table := []validatorTest{
+		{"ok - valid document", func() did.Document {
 			didDoc, _, _ := newDidDoc()
-			a.doc = didDoc
+			return didDoc
 		}, nil},
-		{"nok - verificationMethod ID has no fragment", func(t *testing.T, a *args) {
+		{"nok - verificationMethod ID has no fragment", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			didDoc.VerificationMethod[0].ID.Fragment = ""
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid verificationMethod: ID must have a fragment")},
-		{"nok - verificationMethod ID has wrong prefix", func(t *testing.T, a *args) {
+		{"nok - verificationMethod ID has wrong prefix", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			didDoc.VerificationMethod[0].ID.ID = "foo:123"
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid verificationMethod: ID must have document prefix")},
-		{"nok - verificationMethod with duplicate id", func(t *testing.T, a *args) {
+		{"nok - verificationMethod with duplicate id", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			method := didDoc.VerificationMethod[0]
 			didDoc.VerificationMethod = append(didDoc.VerificationMethod, method)
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid verificationMethod: ID must be unique")},
-		{"nok - verificationMethod with invalid thumbprint", func(t *testing.T, a *args) {
+		{"nok - verificationMethod with invalid thumbprint", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			keyID := didDoc.VerificationMethod[0].ID
 			keyID.Fragment = "foobar"
 			pk, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 			vm, _ := did.NewVerificationMethod(keyID, didDoc.VerificationMethod[0].Type, didDoc.VerificationMethod[0].Controller, pk.Public())
 			didDoc.VerificationMethod = append(didDoc.VerificationMethod, vm)
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid verificationMethod: key thumbprint does not match ID")},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			a := args{}
-			tt.beforeFn(t, &a)
-			if err := (verificationMethodValidator{}).Validate(a.doc); err != nil || tt.wantedErr != nil {
-				if err == nil {
-					if tt.wantedErr != nil {
-						t.Error("expected an error, got nothing")
-
-					}
-				} else {
-					if tt.wantedErr == nil {
-
-						t.Errorf("unexpected error: %v", err)
-					} else {
-						if tt.wantedErr.Error() != err.Error() {
-							t.Errorf("wrong error\ngot:  %v\nwant: %v", err, tt.wantedErr)
-						}
-					}
-				}
-			}
-		})
-	}
+	tableDrivenValidation(t, table, verificationMethodValidator{})
 }
 
-func Test_serviceValidator(t *testing.T) {
-	type args struct {
-		doc did.Document
-	}
-	tests := []struct {
-		name      string
-		beforeFn  func(t *testing.T, a *args)
-		wantedErr error
-	}{
-		{"ok - valid document", func(t *testing.T, a *args) {
+func Test_basicServiceValidator(t *testing.T) {
+	table := []validatorTest{
+		{"ok - valid document", func() did.Document {
 			didDoc, _, _ := newDidDoc()
-			a.doc = didDoc
+			return didDoc
 		}, nil},
-		{"nok - service with duplicate id", func(t *testing.T, a *args) {
+		{"ok - service endpoint is not validated", func() did.Document {
+			// service endpoint is validated in managedServiceValidator
+			didDoc, _, _ := newDidDoc()
+			didDoc.Service[0].ServiceEndpoint = "did:foo:123/serviceEndpoint?type=NutsComm"
+			return didDoc
+		}, nil},
+		{"nok - service with duplicate id", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			svc := didDoc.Service[0]
 			didDoc.Service = append(didDoc.Service, svc)
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid service: ID must be unique")},
-		{"nok - service ID has no fragment", func(t *testing.T, a *args) {
+		{"nok - service ID has no fragment", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			didDoc.Service[0].ID.Fragment = ""
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid service: ID must have a fragment")},
-		{"nok - service ID has wrong prefix", func(t *testing.T, a *args) {
+		{"nok - service ID has wrong prefix", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			uri := ssi.MustParseURI("did:foo:123#foobar")
 			didDoc.Service[0].ID = uri
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid service: ID must have document prefix")},
-		{"nok - service with duplicate type", func(t *testing.T, a *args) {
+		{"nok - service with duplicate type", func() did.Document {
 			didDoc, _, _ := newDidDoc()
 			svc := didDoc.Service[0]
 			svc.ID.Fragment = "foobar"
 			didDoc.Service = append(didDoc.Service, svc)
-			a.doc = didDoc
+			return didDoc
 		}, errors.New("invalid service: service type is duplicate")},
 	}
+	tableDrivenValidation(t, table, basicServiceValidator{})
+}
+
+func Test_managedServiceValidator(t *testing.T) {
+	// table driven testing errors for unexpected (number of) mock calls have poor localisation.
+	// comment out validatorTests in the table to find the culprit.
+	referencedDocument, _, _ := newDidDoc()
+	service := did.Service{Type: "referenced_service", ServiceEndpoint: "https://nuts.nl"}
+	serviceRef := didservice.MakeServiceReference(referencedDocument.ID, service.Type)
+	referencedDocument.Service = append(referencedDocument.Service, service)
+
+	docResolver := types.NewMockDocResolver(gomock.NewController(t))
+	serviceResolver := didservice.NewServiceResolver(docResolver)
+
+	t.Run("basic", func(t *testing.T) {
+		table := []validatorTest{
+			{"ok - valid document", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				return didDoc
+			}, nil},
+			{"ok - doesn't panic", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service = nil
+				return didDoc
+			}, nil},
+			{"ok - resolves string", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].ServiceEndpoint = serviceRef.String()
+
+				docResolver.EXPECT().Resolve(referencedDocument.ID, nil).Return(&referencedDocument, nil, nil)
+
+				return didDoc
+			}, nil},
+			{"ok - normalizes and resolves map", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].ServiceEndpoint = map[string]interface{}{
+					"reference":      serviceRef, // URI must be normalized
+					"url":            "super invalid but isn't validated",
+					"otherReference": serviceRef.String(),
+				}
+
+				docResolver.EXPECT().Resolve(referencedDocument.ID, nil).Return(&referencedDocument, nil, nil)
+
+				return didDoc
+			}, nil},
+			{"ok - self reference", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].ServiceEndpoint = serviceRef.String()
+				didDoc.Service = append(didDoc.Service, did.Service{
+					ID:              ssi.URI{},
+					Type:            "self_reference",
+					ServiceEndpoint: didservice.MakeServiceReference(didDoc.ID, didDoc.Service[0].Type),
+				})
+
+				docResolver.EXPECT().Resolve(referencedDocument.ID, nil).Return(&referencedDocument, nil, nil)
+
+				return didDoc
+			}, nil},
+			{"nok - resolve fails", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].ServiceEndpoint = didservice.MakeServiceReference(referencedDocument.ID, "does_not_exist")
+
+				docResolver.EXPECT().Resolve(referencedDocument.ID, nil).Return(&referencedDocument, nil, nil)
+
+				return didDoc
+			}, errors.New("invalid service: service not found in DID Document")},
+			{"nok - invalid format", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].ServiceEndpoint = []string{serviceRef.String(), serviceRef.String()}
+				return didDoc
+			}, errors.New("invalid service: invalid service format")},
+			{"nok - invalid reference", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].ServiceEndpoint = "did:invalid:reference"
+				return didDoc
+			}, errors.New("invalid service: DID service query invalid: endpoint URI path must be /serviceEndpoint")},
+		}
+		tableDrivenValidation(t, table, managedServiceValidator{serviceResolver})
+	})
+
+	t.Run("NutsComm", func(t *testing.T) {
+		table := []validatorTest{
+			{"ok", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "NutsComm"
+				didDoc.Service[0].ServiceEndpoint = "grpc://nuts.nl:5555"
+				return didDoc
+			}, nil},
+			{"nok - invalid scheme", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "NutsComm"
+				return didDoc
+			}, errors.New("invalid service: NutsComm: scheme must be grpc")},
+			{"nok - validates after resolving", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service = append(didDoc.Service, did.Service{
+					Type:            "NutsComm",
+					ServiceEndpoint: didservice.MakeServiceReference(didDoc.ID, didDoc.Service[0].Type),
+				})
+				return didDoc
+			}, errors.New("invalid service: NutsComm: scheme must be grpc")},
+			{"nok - invalid format", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "NutsComm"
+				didDoc.Service[0].ServiceEndpoint = map[string]string{"NutsComm": "grpc://nuts.nl:5555"}
+				return didDoc
+			}, errors.New("invalid service: NutsComm: endpoint not a string")},
+		}
+		tableDrivenValidation(t, table, managedServiceValidator{serviceResolver})
+	})
+
+	t.Run("node-contact-info", func(t *testing.T) {
+		table := []validatorTest{
+			{"ok - node-contact-info all valid", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "node-contact-info"
+				didDoc.Service[0].ServiceEndpoint = map[string]string{
+					"name":      "name",
+					"email":     "valid@email.address",
+					"telephone": "is a string",
+					"website":   "https://nuts.nl",
+				}
+				return didDoc
+			}, nil},
+			{"ok - minimal info", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "node-contact-info"
+				didDoc.Service[0].ServiceEndpoint = map[string]string{
+					"email": "valid@email.address",
+				}
+				return didDoc
+			}, nil},
+			{"ok - validates after resolving", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0] = did.Service{
+					Type:            "node-contact-info",
+					ServiceEndpoint: didservice.MakeServiceReference(didDoc.ID, "otherService"),
+				}
+				didDoc.Service = append(didDoc.Service, did.Service{
+					Type:            "otherService",
+					ServiceEndpoint: map[string]any{"email": "valid@email.address"}})
+				return didDoc
+			}, nil},
+			{"nok - missing email", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "node-contact-info"
+				didDoc.Service[0].ServiceEndpoint = map[string]string{}
+				return didDoc
+			}, errors.New("invalid service: node-contact-info: missing email")},
+			{"nok - not a map", func() did.Document {
+				didDoc, _, _ := newDidDoc()
+				didDoc.Service[0].Type = "node-contact-info"
+				didDoc.Service[0].ServiceEndpoint = "valid@email.address"
+				return didDoc
+			}, errors.New("invalid service: node-contact-info: not a map")},
+		}
+		tableDrivenValidation(t, table, managedServiceValidator{serviceResolver})
+	})
+}
+
+type validatorTest struct {
+	name        string
+	buildDoc    func() did.Document
+	expectedErr error
+}
+
+func tableDrivenValidation(t *testing.T, tests []validatorTest, validator did.Validator) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a := args{}
-			tt.beforeFn(t, &a)
-			if err := (serviceValidator{}).Validate(a.doc); err != nil || tt.wantedErr != nil {
-				if err == nil {
-					if tt.wantedErr != nil {
-						t.Error("expected an error, got nothing")
-
-					}
-				} else {
-					if tt.wantedErr == nil {
-
-						t.Errorf("unexpected error: %v", err)
-					} else {
-						if tt.wantedErr.Error() != err.Error() {
-							t.Errorf("wrong error\ngot:  %v\nwant: %v", err, tt.wantedErr)
-						}
-					}
-				}
+			err := validator.Validate(tt.buildDoc())
+			if tt.expectedErr == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.expectedErr.Error())
 			}
 		})
 	}
