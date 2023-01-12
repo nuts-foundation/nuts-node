2762f0f1619ebf9c60cd49ae88e3b3a2e92d632b
Rein Krul <info@reinkrul.nl>
2023-01-12 11:53:04 +0100

    tests

--- vcr/ambassador_test.go
+++ vcr/ambassador_test.go
@@ -157,6 +157,30 @@ func TestAmbassador_removeUnrecoverableErrors(t *testing.T) {
 		require.NoError(t, err)
 		assert.Empty(t, logHook.Entries)
 	})
+	t.Run("error - removing event", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		notifier := dag.NewMockNotifier(ctrl)
+		notifier.EXPECT().Name().Return("vcr_vcs")
+		evts := []dag.Event{
+			{Error: "a remoteallowlist error: loading document failed: context not on the remoteallowlist", Hash: hash.RandomHash()},
+		}
+		notifier.EXPECT().GetFailedEvents().Return(evts, nil)
+		notifier.EXPECT().Finished(gomock.Any()).Return(errors.New("b00m!"))
+
+		err := (&ambassador{}).removeUnrecoverableErrors([]dag.Notifier{notifier})
+
+		require.Error(t, err)
+	})
+	t.Run("error - getting failed events", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		notifier := dag.NewMockNotifier(ctrl)
+		notifier.EXPECT().Name().Return("vcr_vcs")
+		notifier.EXPECT().GetFailedEvents().Return(nil, errors.New("b00m!"))
+
+		err := (&ambassador{}).removeUnrecoverableErrors([]dag.Notifier{notifier})
+
+		require.Error(t, err)
+	})
 }
 
 func TestAmbassador_handleReprocessEvent(t *testing.T) {
