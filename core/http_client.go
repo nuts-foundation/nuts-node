a2c3d4591b3728b6ac0b6c5218860018cd78ed98
Wout Slakhorst <wout.slakhorst@nedap.com>
2023-02-14 15:08:43 +0100

    comments

--- core/http_client.go
+++ core/http_client.go
@@ -74,6 +74,7 @@ func (w httpRequestDoerAdapter) Do(req *http.Request) (*http.Response, error) {
 // This does not use the generated client options for e.g. authentication,
 // because each generated OpenAPI client reimplements the client options using structs,
 // which makes them incompatible with each other, making it impossible to use write generic client code for common traits like authorization.
+// If the given authorization token builder is non-nil, it calls it and passes the resulting token as bearer token with requests.
 func CreateHTTPClient(cfg ClientConfig, generator AuthorizationTokenGenerator) (HTTPRequestDoer, error) {
 	var result *httpRequestDoerAdapter
 	client := &http.Client{}
