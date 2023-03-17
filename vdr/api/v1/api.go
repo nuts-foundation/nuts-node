564e648cd6b6991e99c8244aced7c276f6bce7fe
reinkrul <info@reinkrul.nl>
2023-03-17 11:50:22 +0100

    VDR: Strict OpenAPI interfaces (#1807)

--- vdr/api/v1/api.go
+++ vdr/api/v1/api.go
@@ -20,8 +20,9 @@
 package v1
 
 import (
+	"context"
 	"fmt"
-	httpModule "github.com/nuts-foundation/nuts-node/http"
+	"github.com/nuts-foundation/nuts-node/audit"
 	"github.com/nuts-foundation/nuts-node/vdr"
 	"net/http"
 	"time"
@@ -35,7 +36,7 @@ import (
 	"github.com/nuts-foundation/nuts-node/vdr/types"
 )
 
-var _ ServerInterface = (*Wrapper)(nil)
+var _ StrictServerInterface = (*Wrapper)(nil)
 var _ core.ErrorStatusCodeResolver = (*Wrapper)(nil)
 
 // Wrapper is needed to connect the implementation to the echo ServiceWrapper
@@ -58,100 +59,104 @@ func (a *Wrapper) ResolveStatusCode(err error) int {
 	})
 }
 
-// Preprocess is called just before the API operation itself is invoked.
-func (a *Wrapper) Preprocess(operationID string, context echo.Context) {
-	httpModule.Preprocess(context, a, vdr.ModuleName, operationID)
-}
-
 // DeleteVerificationMethod accepts a DID and a KeyIdentifier of a verificationMethod and calls the DocManipulator
 // to remove the verificationMethod from the given document.
-func (a *Wrapper) DeleteVerificationMethod(ctx echo.Context, didStr string, kidStr string) error {
-	id, err := did.ParseDID(didStr)
+func (a *Wrapper) DeleteVerificationMethod(ctx context.Context, request DeleteVerificationMethodRequestObject) (DeleteVerificationMethodResponseObject, error) {
+	id, err := did.ParseDID(request.Did)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	kid, err := did.ParseDIDURL(kidStr)
+	kid, err := did.ParseDIDURL(request.Kid)
 	if err != nil {
-		return core.InvalidInputError("given kid could not be parsed: %w", err)
+		return nil, core.InvalidInputError("given kid could not be parsed: %w", err)
 	}
 
-	err = a.DocManipulator.RemoveVerificationMethod(ctx.Request().Context(), *id, *kid)
+	err = a.DocManipulator.RemoveVerificationMethod(ctx, *id, *kid)
 	if err != nil {
-		return fmt.Errorf("could not remove verification method from document: %w", err)
+		return nil, fmt.Errorf("could not remove verification method from document: %w", err)
 	}
-	return ctx.NoContent(http.StatusNoContent)
+	return DeleteVerificationMethod204Response{}, nil
 }
 
 // AddNewVerificationMethod accepts a DID and adds a new VerificationMethod to that corresponding document.
-func (a *Wrapper) AddNewVerificationMethod(ctx echo.Context, id string) error {
-	d, err := did.ParseDID(id)
+func (a *Wrapper) AddNewVerificationMethod(ctx context.Context, request AddNewVerificationMethodRequestObject) (AddNewVerificationMethodResponseObject, error) {
+	d, err := did.ParseDID(request.Did)
 	if err != nil {
-		return err
-	}
-	req := AddNewVerificationMethodJSONRequestBody{}
-	if err := ctx.Bind(&req); err != nil {
-		return err
+		return nil, err
 	}
 
-	vm, err := a.DocManipulator.AddVerificationMethod(ctx.Request().Context(), *d, req.ToFlags(vdrDoc.DefaultCreationOptions().KeyFlags))
-	if err != nil {
-		return err
+	opts := request.Body
+	if opts == nil {
+		opts = &VerificationMethodRelationship{}
 	}
-	return ctx.JSON(http.StatusOK, *vm)
+
+	vm, err := a.DocManipulator.AddVerificationMethod(ctx, *d, opts.ToFlags(vdrDoc.DefaultCreationOptions().KeyFlags))
+	if err != nil {
+		return nil, err
+	}
+	return AddNewVerificationMethod200JSONResponse(*vm), nil
 }
 
 func (a *Wrapper) Routes(router core.EchoRouter) {
-	RegisterHandlers(router, a)
+	RegisterHandlers(router, NewStrictHandler(a, []StrictMiddlewareFunc{
+		func(f StrictHandlerFunc, operationID string) StrictHandlerFunc {
+			return func(ctx echo.Context, request interface{}) (response interface{}, err error) {
+				ctx.Set(core.OperationIDContextKey, operationID)
+				ctx.Set(core.ModuleNameContextKey, vdr.ModuleName)
+				ctx.Set(core.StatusCodeResolverContextKey, a)
+				return f(ctx, request)
+			}
+		},
+		func(f StrictHandlerFunc, operationID string) StrictHandlerFunc {
+			return audit.StrictMiddleware(f, vdr.ModuleName, operationID)
+		},
+	}))
 }
 
 // CreateDID creates a new DID Document and returns it.
-func (a *Wrapper) CreateDID(ctx echo.Context) error {
-	req := DIDCreateRequest{}
-	if err := ctx.Bind(&req); err != nil {
-		return err
-	}
-
+func (a *Wrapper) CreateDID(ctx context.Context, request CreateDIDRequestObject) (CreateDIDResponseObject, error) {
 	options := vdrDoc.DefaultCreationOptions()
-	if req.Controllers != nil {
-		for _, c := range *req.Controllers {
+	if request.Body.Controllers != nil {
+		for _, c := range *request.Body.Controllers {
 			id, err := did.ParseDID(c)
 			if err != nil {
-				return core.InvalidInputError("controller entry (%s) could not be parsed: %w", c, err)
+				return nil, core.InvalidInputError("controller entry (%s) could not be parsed: %w", c, err)
 			}
 			options.Controllers = append(options.Controllers, *id)
 		}
 	}
-	options.KeyFlags = req.VerificationMethodRelationship.ToFlags(options.KeyFlags)
-	if req.SelfControl != nil {
-		options.SelfControl = *req.SelfControl
+	options.KeyFlags = request.Body.VerificationMethodRelationship.ToFlags(options.KeyFlags)
+	if request.Body.SelfControl != nil {
+		options.SelfControl = *request.Body.SelfControl
 	}
 
-	doc, _, err := a.VDR.Create(ctx.Request().Context(), options)
+	doc, _, err := a.VDR.Create(ctx, options)
 	// if this operation leads to an error, it may return a 500
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// this API returns a DIDDocument according to spec, so it may return the business object
-	return ctx.JSON(http.StatusOK, *doc)
+	return CreateDID200JSONResponse(*doc), nil
 }
 
 // GetDID returns a DID document and DID document metadata based on a DID.
-func (a *Wrapper) GetDID(ctx echo.Context, targetDID string, params GetDIDParams) error {
-	d, err := did.ParseDID(targetDID)
+func (a *Wrapper) GetDID(ctx context.Context, request GetDIDRequestObject) (GetDIDResponseObject, error) {
+	d, err := did.ParseDID(request.Did)
 	if err != nil {
-		return core.InvalidInputError("given did is not valid: %w", err)
+		return nil, core.InvalidInputError("given did is not valid: %w", err)
 	}
 	resolverMetadata := &types.ResolveMetadata{AllowDeactivated: true}
 
+	params := request.Params
 	if params.VersionId != nil {
 		if params.VersionTime != nil {
-			return core.InvalidInputError("versionId and versionTime are mutually exclusive")
+			return nil, core.InvalidInputError("versionId and versionTime are mutually exclusive")
 		}
 		versionHash, err := hash.ParseHex(*params.VersionId)
 		if err != nil {
-			return core.InvalidInputError("given hash is not valid: %w", err)
+			return nil, core.InvalidInputError("given hash is not valid: %w", err)
 		}
 		resolverMetadata.Hash = &versionHash
 	}
@@ -159,14 +164,14 @@ func (a *Wrapper) GetDID(ctx echo.Context, targetDID string, params GetDIDParams
 	if params.VersionTime != nil {
 		versionTime, err := time.Parse(time.RFC3339, *params.VersionTime)
 		if err != nil {
-			return core.InvalidInputError("versionTime has invalid format: %w", err)
+			return nil, core.InvalidInputError("versionTime has invalid format: %w", err)
 		}
 		resolverMetadata.ResolveTime = &versionTime
 	}
 
 	doc, meta, err := a.DocResolver.Resolve(*d, resolverMetadata)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	resolutionResult := DIDResolutionResult{
@@ -174,14 +179,14 @@ func (a *Wrapper) GetDID(ctx echo.Context, targetDID string, params GetDIDParams
 		DocumentMetadata: *meta,
 	}
 
-	return ctx.JSON(http.StatusOK, resolutionResult)
+	return GetDID200JSONResponse(resolutionResult), nil
 }
 
-func (a *Wrapper) ConflictedDIDs(ctx echo.Context) error {
+func (a *Wrapper) ConflictedDIDs(_ context.Context, _ ConflictedDIDsRequestObject) (ConflictedDIDsResponseObject, error) {
 	docs, metas, err := a.VDR.ConflictedDocuments()
 	if err != nil {
 		// 500 internal server error
-		return err
+		return nil, err
 	}
 
 	// []docs, []meta to [](doc, meta)
@@ -193,38 +198,33 @@ func (a *Wrapper) ConflictedDIDs(ctx echo.Context) error {
 		}
 	}
 
-	return ctx.JSON(http.StatusOK, returnValues)
+	return ConflictedDIDs200JSONResponse(returnValues), nil
 }
 
 // UpdateDID updates a DID Document given a DID and DID Document body. It returns the updated DID Document.
-func (a *Wrapper) UpdateDID(ctx echo.Context, targetDID string) error {
-	d, err := did.ParseDID(targetDID)
+func (a *Wrapper) UpdateDID(ctx context.Context, request UpdateDIDRequestObject) (UpdateDIDResponseObject, error) {
+	d, err := did.ParseDID(request.Did)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	req := DIDUpdateRequest{}
-	if err := ctx.Bind(&req); err != nil {
-		return err
-	}
-
-	err = a.VDR.Update(ctx.Request().Context(), *d, req.Document)
+	err = a.VDR.Update(ctx, *d, request.Body.Document)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return ctx.JSON(http.StatusOK, req.Document)
+	return UpdateDID200JSONResponse(request.Body.Document), nil
 }
 
 // DeactivateDID deactivates a DID Document given a DID.
 // It returns a 200 and an empty body if the deactivation was successful.
-func (a *Wrapper) DeactivateDID(ctx echo.Context, targetDID string) error {
-	id, err := did.ParseDID(targetDID)
+func (a *Wrapper) DeactivateDID(ctx context.Context, request DeactivateDIDRequestObject) (DeactivateDIDResponseObject, error) {
+	id, err := did.ParseDID(request.Did)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	err = a.DocManipulator.Deactivate(ctx.Request().Context(), *id)
+	err = a.DocManipulator.Deactivate(ctx, *id)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return ctx.NoContent(http.StatusOK)
+	return &DeactivateDID200Response{}, nil
 }
