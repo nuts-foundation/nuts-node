cc21ae5242d9dfa3a0cf618fe20f26f75780f651
Rein Krul <info@reinkrul.nl>
2023-04-07 08:41:27 +0200

    removed unncessaary escape (done by OpenAPI server)

--- vcr/api/oidc4vci_v0/wallet.go
+++ vcr/api/oidc4vci_v0/wallet.go
@@ -6,7 +6,6 @@ import (
 	"github.com/nuts-foundation/go-did/did"
 	"github.com/nuts-foundation/nuts-node/core"
 	"github.com/nuts-foundation/nuts-node/vcr/api/oidc4vci_v0/types"
-	"net/url"
 )
 
 func (w Wrapper) GetOAuth2ClientMetadata(_ context.Context, request GetOAuth2ClientMetadataRequestObject) (GetOAuth2ClientMetadataResponseObject, error) {
@@ -22,12 +21,8 @@ func (w Wrapper) CredentialOffer(ctx context.Context, request CredentialOfferReq
 	if err != nil {
 		return nil, core.NotFoundError("invalid DID")
 	}
-	offerParam, err := url.QueryUnescape(request.Params.CredentialOffer)
-	if err != nil {
-		return nil, core.InvalidInputError("unable to unescape credential offer query param")
-	}
 	offer := types.CredentialOffer{}
-	if json.Unmarshal([]byte(offerParam), &offer) != nil {
+	if json.Unmarshal([]byte(request.Params.CredentialOffer), &offer) != nil {
 		return nil, core.InvalidInputError("unable to unmarshal credential offer query param")
 	}
 
