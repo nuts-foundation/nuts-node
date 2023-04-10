2a2a3a2eaf954588cc8f174b18adda4f97948274
Rein Krul <info@reinkrul.nl>
2023-04-10 11:47:29 +0200

    docs

--- vcr/oidc4vci/package.go
+++ vcr/oidc4vci/package.go
@@ -16,16 +16,5 @@
  *
  */
 
-package types
-
-import (
-	"github.com/nuts-foundation/go-did/vc"
-	"time"
-)
-
-// Writer is the interface that groups al the VC write methods
-type Writer interface {
-	// StoreCredential writes a VC to storage. Before writing, it calls Verify!
-	// It can handle duplicates.
-	StoreCredential(vc vc.VerifiableCredential, validAt *time.Time) error
-}
+// Package oidc4vci provides clients for interacting with OIDC4VCI wallets and issuers.
+package oidc4vci
