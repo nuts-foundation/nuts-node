9463735adeedc22e2c6de23303912532cb72f1a2
Rein Krul <info@reinkrul.nl>
2023-04-24 13:43:31 +0200

    Fix test

--- vcr/oidc4vci/error_test.go
+++ vcr/oidc4vci/error_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestError_Error(t *testing.T) {
 	t.Run("with underlying error", func(t *testing.T) {
-		assert.EqualError(t, Error{Err: errors.New("token has expired"), Code: InvalidToken}, "invalid_token: token has expired")
+		assert.EqualError(t, Error{Err: errors.New("token has expired"), Code: InvalidToken}, "invalid_token - token has expired")
 	})
 	t.Run("without underlying error", func(t *testing.T) {
 		assert.EqualError(t, Error{Code: InvalidToken}, "invalid_token")
