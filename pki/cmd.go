5b169b14acf16b7be9f5a6868f02a61926ab1468
Gerard Snaauw <gerard.snaauw@nedap.com>
2023-05-23 10:09:17 +0200

    pr feedback + copyright

--- pki/cmd.go
+++ pki/cmd.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2023 Nuts community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package pki
 
 import (
@@ -11,8 +29,8 @@ func FlagSet() *pflag.FlagSet {
 	flagSet := pflag.NewFlagSet("pki", pflag.ContinueOnError)
 
 	// Flags for denylist features
-	flagSet.Int("pki.maxupdatefailhours", defs.MaxUpdateFailHours, "maximum number of hours that a denylist update can fail")
-	flagSet.Bool("pki.softfail", defs.Softfail, "do not reject certificates if their revocation status cannot be established when softfail is true")
+	flagSet.Int("pki.maxupdatefailhours", defs.MaxUpdateFailHours, "Maximum number of hours that a denylist update can fail")
+	flagSet.Bool("pki.softfail", defs.Softfail, "Do not reject certificates if their revocation status cannot be established when softfail is true")
 	// TODO: Choose a default trusted signer key
 	flagSet.String("pki.denylist.trustedsigner", defs.Denylist.TrustedSigner, "Ed25519 public key (in PEM format) of the trusted signer for denylists")
 	// TODO: Choose a default denylist URL
