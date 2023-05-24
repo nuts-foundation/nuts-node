fa233951d928dbc7128ddc4ffb26ab2c6ea56ce5
beardedfoo <773275+beardedfoo@users.noreply.github.com>
2023-05-24 13:06:47 +0000

    Enhance denylist logging

--- pki/denylist.go
+++ pki/denylist.go
@@ -135,6 +135,7 @@ func (b *denylistImpl) ValidateCert(cert *x509.Certificate) error {
 			logger().WithError(err).
 				WithField("Issuer", issuer).
 				WithField("S/N", serialNumber).
+				WithField("Thumbprint", thumbprint).
 				Error("Cert validation failed because the denylist cannot be downloaded")
 
 			// Return an error indicating the denylist cannot be retrieved
@@ -156,10 +157,22 @@ func (b *denylistImpl) ValidateCert(cert *x509.Certificate) error {
 	for _, entry := range *entriesPtr {
 		// Check for this issuer and serial number combination
 		if entry.Issuer == issuer && entry.SerialNumber == serialNumber && entry.JWKThumbprint == thumbprint {
+			logger().
+				WithField("Issuer", issuer).
+				WithField("S/N", serialNumber).
+				WithField("Thumbprint", thumbprint).
+				Debug("Rejecting banned certificate")
 			// Return an error indicating the certificate has been denylisted
 			return fmt.Errorf("%w: %s", ErrCertBanned, entry.Reason)
 		}
 	}
+
+	// Log a message about the cert being validated
+	logger().
+		WithField("Issuer", issuer).
+		WithField("S/N", serialNumber).
+		WithField("Thumbprint", thumbprint).
+		Debug("Validated certificate")
 
 	// Return a nil error as the certificate hasn't been denylisted
 	return nil
@@ -206,6 +219,9 @@ func (b *denylistImpl) Update() error {
 
 	// Track when the denylist was last updated
 	b.lastUpdated = time.Now()
+
+	// Log when the denylist is updated
+	logger().Debug("Denylist updated successfully")
 
 	// Return a nil error as the denylist was successfully updated
 	return nil
