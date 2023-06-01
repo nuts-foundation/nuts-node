2fcd7da46decc74eaa05296cb1428afdcc69bd2e
Rein Krul <info@reinkrul.nl>
2023-06-01 13:20:20 +0200

    testfixc

--- vcr/vcr_test.go
+++ vcr/vcr_test.go
@@ -21,6 +21,7 @@ package vcr
 
 import (
 	"encoding/json"
+	"github.com/nuts-foundation/nuts-node/pki"
 	"github.com/nuts-foundation/nuts-node/storage"
 	"github.com/stretchr/testify/require"
 	"os"
@@ -69,7 +70,10 @@ func TestVCR_Configure(t *testing.T) {
 		})
 		t.Run("HTTP allowed in non-strict mode", func(t *testing.T) {
 			testDirectory := io.TestDirectory(t)
-			instance := NewVCRInstance(nil, nil, nil, nil, jsonld.NewTestJSONLDManager(t), nil, storage.NewTestStorageEngine(testDirectory), nil).(*vcr)
+			ctrl := gomock.NewController(t)
+			pkiProvider := pki.NewMockProvider(ctrl)
+			pkiProvider.EXPECT().CreateClientTLSConfig(gomock.Any()).Return(nil, nil).AnyTimes()
+			instance := NewVCRInstance(nil, nil, nil, nil, jsonld.NewTestJSONLDManager(t), nil, storage.NewTestStorageEngine(testDirectory), pkiProvider).(*vcr)
 			instance.config.OIDC4VCI.Enabled = true
 			instance.config.OIDC4VCI.URL = "http://example.com"
 
