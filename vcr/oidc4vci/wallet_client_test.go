3f2f6c31428bead6bc71cd660d1ea7403457178f
Rein Krul <info@reinkrul.nl>
2023-06-07 16:09:19 +0200

    fix2

--- vcr/oidc4vci/wallet_client_test.go
+++ vcr/oidc4vci/wallet_client_test.go
@@ -85,7 +85,7 @@ func Test_httpWalletClient_OfferCredential(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, setup.issuerMetadata.CredentialIssuer, credentialOffer["credential_issuer"])
 		require.Equal(t, []interface{}{map[string]interface{}{"issuer": "issuer"}}, credentialOffer["credentials"])
-		require.Equal(t, []interface{}{map[string]interface{}{"grant_type": "pre-authorized_code"}}, credentialOffer["grants"])
+		require.Equal(t, map[string]interface{}{"grant_type": "pre-authorized_code"}, credentialOffer["grants"])
 	})
 	t.Run("error", func(t *testing.T) {
 		setup := setupClientTest(t)
