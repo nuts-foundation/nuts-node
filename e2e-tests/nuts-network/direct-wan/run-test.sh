8473d085fd75b375bc088b00521044d6229ff6d7
Rein Krul <info@reinkrul.nl>
2023-07-03 13:35:24 +0200

    feedback

--- e2e-tests/nuts-network/direct-wan/run-test.sh
+++ e2e-tests/nuts-network/direct-wan/run-test.sh
@@ -19,9 +19,6 @@ echo "------------------------------------"
 # Wait for Nuts Network nodes to build connections
 waitForDiagnostic "nodeA" connected_peers_count 1
 waitForDiagnostic "nodeB" connected_peers_count 1
-# Assert that node A is connected to B and vice versa using diagnostics. It should look something like this:
-assertDiagnostic "http://localhost:11323" "connected_peers_count: 1"
-assertDiagnostic "http://localhost:21323" "connected_peers_count: 1"
 
 echo "------------------------------------"
 echo "Stopping Docker containers..."
