69a594fb5d6bce77012d7718ac4620301e671b5a
Rein Krul <info@reinkrul.nl>
2023-07-20 10:49:20 +0200

    mock

--- vdr/types/mock.go
+++ vdr/types/mock.go
@@ -17,6 +17,45 @@ import (
 	gomock "go.uber.org/mock/gomock"
 )
 
+// MockDIDResolver is a mock of DIDResolver interface.
+type MockDIDResolver struct {
+	ctrl     *gomock.Controller
+	recorder *MockDIDResolverMockRecorder
+}
+
+// MockDIDResolverMockRecorder is the mock recorder for MockDIDResolver.
+type MockDIDResolverMockRecorder struct {
+	mock *MockDIDResolver
+}
+
+// NewMockDIDResolver creates a new mock instance.
+func NewMockDIDResolver(ctrl *gomock.Controller) *MockDIDResolver {
+	mock := &MockDIDResolver{ctrl: ctrl}
+	mock.recorder = &MockDIDResolverMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use.
+func (m *MockDIDResolver) EXPECT() *MockDIDResolverMockRecorder {
+	return m.recorder
+}
+
+// Resolve mocks base method.
+func (m *MockDIDResolver) Resolve(id did.DID, metadata *ResolveMetadata) (*did.Document, *DocumentMetadata, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Resolve", id, metadata)
+	ret0, _ := ret[0].(*did.Document)
+	ret1, _ := ret[1].(*DocumentMetadata)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// Resolve indicates an expected call of Resolve.
+func (mr *MockDIDResolverMockRecorder) Resolve(id, metadata interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Resolve", reflect.TypeOf((*MockDIDResolver)(nil).Resolve), id, metadata)
+}
+
 // MockDocResolver is a mock of DocResolver interface.
 type MockDocResolver struct {
 	ctrl     *gomock.Controller
