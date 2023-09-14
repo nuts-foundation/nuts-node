a6e2f5d7dfc761a1f701fc4daef613e0f0c305f5
Gerard Snaauw <33763579+gerardsn@users.noreply.github.com>
2023-09-14 16:06:21 +0200

    Create well-known path from oauth issuer (#2489)
    * create well-known path from issuer
    
    * pr feedback

--- auth/api/iam/metadata_test.go
+++ auth/api/iam/metadata_test.go
@@ -20,9 +20,49 @@ package iam
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/url"
 	"testing"
 )
+
+func TestIssuerIdToWellKnown(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		issuer := "https://nuts.nl/iam/id"
+		u, err := IssuerIdToWellKnown(issuer, authzServerWellKnown, true)
+		require.NoError(t, err)
+		assert.Equal(t, "https://nuts.nl/.well-known/oauth-authorization-server/iam/id", u.String())
+	})
+	t.Run("no path in issuer", func(t *testing.T) {
+		issuer := "https://nuts.nl"
+		u, err := IssuerIdToWellKnown(issuer, authzServerWellKnown, true)
+		require.NoError(t, err)
+		assert.Equal(t, "https://nuts.nl/.well-known/oauth-authorization-server", u.String())
+	})
+	t.Run("don't unescape path", func(t *testing.T) {
+		issuer := "https://nuts.nl/iam/%2E%2E/still-has-iam"
+		u, err := IssuerIdToWellKnown(issuer, authzServerWellKnown, true)
+		require.NoError(t, err)
+		assert.Equal(t, "https://nuts.nl/.well-known/oauth-authorization-server/iam/%2E%2E/still-has-iam", u.String())
+	})
+	t.Run("https in strictmode", func(t *testing.T) {
+		issuer := "http://nuts.nl/iam/id"
+		u, err := IssuerIdToWellKnown(issuer, authzServerWellKnown, true)
+		assert.ErrorContains(t, err, "scheme must be https")
+		assert.Nil(t, u)
+	})
+	t.Run("no IP allowed", func(t *testing.T) {
+		issuer := "http://127.0.0.1/iam/id"
+		u, err := IssuerIdToWellKnown(issuer, authzServerWellKnown, false)
+		assert.ErrorContains(t, err, "hostname is IP")
+		assert.Nil(t, u)
+	})
+	t.Run("invalid URL", func(t *testing.T) {
+		issuer := "http:// /iam/id"
+		u, err := IssuerIdToWellKnown(issuer, authzServerWellKnown, true)
+		assert.ErrorContains(t, err, "invalid character \" \" in host name")
+		assert.Nil(t, u)
+	})
+}
 
 func Test_authorizationServerMetadata(t *testing.T) {
 	identity := "https://example.com/iam/did:nuts:123"
