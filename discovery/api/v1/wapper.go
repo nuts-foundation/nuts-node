b6d9e6a2de6fd7fa9e390b920e441adf2cf3ce22
Rein Krul <info@reinkrul.nl>
2023-12-08 17:15:52 +0100

    chores

--- discovery/api/v1/wapper.go
+++ discovery/api/v1/wapper.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2023 Nuts community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package v1
 
 import (
@@ -12,12 +30,10 @@ import (
 var _ StrictServerInterface = (*Wrapper)(nil)
 var _ core.ErrorStatusCodeResolver = (*Wrapper)(nil)
 
-// Wrapper implements the generated interface from oapi-codegen
 type Wrapper struct {
 	Server discovery.Server
 }
 
-// ResolveStatusCode maps errors returned by this API to specific HTTP status codes.
 func (w *Wrapper) ResolveStatusCode(err error) int {
 	// todo
 	switch {
@@ -30,7 +46,6 @@ func (w *Wrapper) ResolveStatusCode(err error) int {
 	}
 }
 
-// Routes registers the routes from the open api spec to the echo router.
 func (w *Wrapper) Routes(router core.EchoRouter) {
 	RegisterHandlers(router, NewStrictHandler(w, []StrictMiddlewareFunc{
 		func(f StrictHandlerFunc, operationID string) StrictHandlerFunc {
