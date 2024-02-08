916732ebce5f65890d7f1f7b101eaf1cd1f89f1e
Roland Groen <roland@edia.nl>
2024-02-08 22:43:04 +0100

    Refactor AccessTokenOid4vci to return pointer to response
    Modified the AccessTokenOid4vci method in the auth IAM API to return a pointer to the Oid4vciTokenResponse object instead of the object itself. This change allows enhanced error handling and reduces memory edge usage by returning a nil pointer in the case of errors, ensuring return data is truly atomic and matches the expected signature. The change also adds more clarity and consistency in function signatures across the codebase.

--- auth/client/iam/client_mitzxnuts.go
+++ auth/client/iam/client_mitzxnuts.go
@@ -87,13 +87,11 @@ func (hb HTTPClient) OpenIdCredentialIssuerMetadata(ctx context.Context, webDID 
 	return &metadata, nil
 }
 
-func (hb HTTPClient) AccessTokenOid4vci(ctx context.Context, clientId string, tokenEndpoint string, redirectUri string, code string, pkceCodeVerifier *string) (oauth.Oid4vciTokenResponse, error) {
-	var token oauth.Oid4vciTokenResponse
+func (hb HTTPClient) AccessTokenOid4vci(ctx context.Context, clientId string, tokenEndpoint string, redirectUri string, code string, pkceCodeVerifier *string) (*oauth.Oid4vciTokenResponse, error) {
 	presentationDefinitionURL, err := url.Parse(tokenEndpoint)
 	if err != nil {
-		return token, err
+		return nil, err
 	}
-
 	// create a POST request with x-www-form-urlencoded body
 	data := url.Values{}
 	data.Set("client_id", clientId)
@@ -107,41 +105,43 @@ func (hb HTTPClient) AccessTokenOid4vci(ctx context.Context, clientId string, to
 	request.Header.Add("Accept", "application/json")
 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	if err != nil {
-		return token, err
+		return nil, err
 	}
 	response, err := hb.httpClient.Do(request.WithContext(ctx))
 	if err != nil {
-		return token, fmt.Errorf("failed to call endpoint: %w", err)
+		return nil, fmt.Errorf("failed to call endpoint: %w", err)
 	}
 	if err = core.TestResponseCode(http.StatusOK, response); err != nil {
 		// check for oauth error
 		if innerErr := core.TestResponseCode(http.StatusBadRequest, response); innerErr != nil {
 			// a non oauth error, the response body could contain a lot of stuff. We'll log and return the entire error
 			log.Logger().Debugf("authorization server token endpoint returned non oauth error (statusCode=%d)", response.StatusCode)
-			return token, err
+			return nil, err
 		}
 		httpErr := err.(core.HttpError)
 		oauthError := oauth.OAuth2Error{}
 		if err := json.Unmarshal(httpErr.ResponseBody, &oauthError); err != nil {
-			return token, fmt.Errorf("unable to unmarshal OAuth error response: %w", err)
+			return nil, fmt.Errorf("unable to unmarshal OAuth error response: %w", err)
 		}
 
-		return token, oauthError
+		return nil, oauthError
 	}
 
 	var responseData []byte
 	if responseData, err = io.ReadAll(response.Body); err != nil {
-		return token, fmt.Errorf("unable to read response: %w", err)
+		return nil, fmt.Errorf("unable to read response: %w", err)
 	}
+
+	var token oauth.Oid4vciTokenResponse
 	if err = json.Unmarshal(responseData, &token); err != nil {
 		// Cut off the response body to 100 characters max to prevent logging of large responses
 		responseBodyString := string(responseData)
 		if len(responseBodyString) > 100 {
 			responseBodyString = responseBodyString[:100] + "...(clipped)"
 		}
-		return token, fmt.Errorf("unable to unmarshal response: %w, %s", err, string(responseData))
+		return nil, fmt.Errorf("unable to unmarshal response: %w, %s", err, string(responseData))
 	}
-	return token, nil
+	return &token, nil
 }
 
 type CredentialRequest struct {
