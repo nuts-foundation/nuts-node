9b10c0f01aa3519e7878180bb17c9cb2c298aa4e
Rein Krul <info@reinkrul.nl>
2024-02-21 13:12:11 +0100

    feedback

--- storage/sql_migrations/007_wallet.sql
+++ storage/sql_migrations/007_wallet.sql
@@ -4,8 +4,7 @@ create table wallet_credential
     holder_did    varchar(500) not null,
     credential_id varchar(500) not null,
     primary key (holder_did, credential_id),
-    constraint fk_wallet_credential foreign key (credential_id) references credential (id),
-    constraint fk_wallet_holder foreign key (holder_did) references vdr_didweb (did)
+    constraint fk_wallet_credential foreign key (credential_id) references credential (id)
 );
 
 create index idx_wallet_holder_did on wallet_credential (holder_did);
