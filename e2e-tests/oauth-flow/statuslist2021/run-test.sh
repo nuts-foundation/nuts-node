f75b8349cf1bd24cf9206df8ae7aa9304bf9ca83
Gerard Snaauw <gerard.snaauw@nedap.com>
2024-03-06 16:50:29 +0100

    add e2e test

---
+++ e2e-tests/oauth-flow/statuslist2021/run-test.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+source ../../util.sh
+
+echo "------------------------------------"
+echo "Cleaning up running Docker containers and volumes, and key material..."
+echo "------------------------------------"
+docker compose down
+docker compose rm -f -v
+rm -rf ./node-*/data
+mkdir ./node-A/data ./node-B/data  # 'data' dirs will be created with root owner by docker if they do not exit. This creates permission issues on CI.
+
+echo "------------------------------------"
+echo "Starting Docker containers..."
+echo "------------------------------------"
+docker compose up -d --remove-orphans
+docker compose up --wait nodeA nodeB
+
+echo "------------------------------------"
+echo "Registering vendors..."
+echo "------------------------------------"
+
+# Register Party A
+PARTY_A_DIDDOC=$(curl -s -X POST http://localhost:11323/internal/vdr/v2/did)
+PARTY_A_DID=$(echo $PARTY_A_DIDDOC | jq -r .id)
+echo "  Vendor A DID: $PARTY_A_DID"
+
+# Register Vendor B
+PARTY_B_DIDDOC=$(curl -s -X POST http://localhost:21323/internal/vdr/v2/did)
+PARTY_B_DID=$(echo $PARTY_B_DIDDOC | jq -r .id)
+echo "  Vendor B DID: $PARTY_B_DID"
+
+echo "---------------------------------------"
+echo "Issuing NutsOrganizationCredential..."
+echo "---------------------------------------"
+
+# Issue NutsOrganizationCredential for Vendor B with a revocable StatusList2021Entry in VC.credentialStatus
+REQUEST="{\"type\":\"NutsOrganizationCredential\",\"issuer\":\"${PARTY_B_DID}\", \"credentialSubject\": {\"id\":\"${PARTY_B_DID}\", \"organization\":{\"name\":\"Caresoft B.V.\", \"city\":\"Caretown\"}},\"withStatusList2021Revocation\": true}"
+RESPONSE=$(echo $REQUEST | curl -s -X POST --data-binary @- http://localhost:21323/internal/vcr/v2/issuer/vc -H "Content-Type:application/json")
+if echo $RESPONSE | grep -q "VerifiableCredential"; then
+  CREDENTIAL_ID=$( echo $RESPONSE | jq .id  | sed "s/\"//g" )
+  echo "  VC issued: $CREDENTIAL_ID"
+else
+  echo "  FAILED: Could not issue NutsOrganizationCredential to node-B" 1>&2
+  echo $RESPONSE
+  exitWithDockerLogs 1
+fi
+
+VALIDATION_REQUEST="{\"verifiableCredential\": ${RESPONSE}}"
+
+echo "---------------------------------------"
+echo "Validating credential pt 1..."
+echo "---------------------------------------"
+
+RESPONSE=$(echo $VALIDATION_REQUEST | curl -s -X POST --data-binary @- http://localhost:21323/internal/vcr/v2/verifier/vc -H "Content-Type:application/json")
+if [[ $( echo $RESPONSE | jq -r .validity ) ==  "true" ]]; then
+  echo "  VC considered valid by node-B"
+else
+  echo "  FAILED: Could not validate NutsOrganizationCredential in node-B wallet" 1>&2
+  echo $RESPONSE
+  exitWithDockerLogs 1
+fi
+
+echo "---------------------------------------"
+echo "Revoking NutsOrganizationCredential..."
+echo "---------------------------------------"
+
+revokeCredential "http://localhost:21323" "${CREDENTIAL_ID}"
+
+echo "---------------------------------------"
+echo "Validating credential pt 2..."
+echo "---------------------------------------"
+
+# confirm revoked by StatusList2021Credential on node-B, uses internal store
+RESPONSE=$(echo $VALIDATION_REQUEST | curl -s -X POST --data-binary @- http://localhost:21323/internal/vcr/v2/verifier/vc -H "Content-Type:application/json")
+if [[ $( echo $RESPONSE | jq -r .validity ) ==  "false" ]]; then
+  echo "  VC considered revoked by node-B"
+else
+  echo "  FAILED: Credential not revoked on node-B" 1>&2
+  echo $RESPONSE
+  exitWithDockerLogs 1
+fi
+
+# confirm revoked by StatusList2021Credential on node-A, fetches StatusList2021Credential from node-B using APIs
+RESPONSE=$(echo $VALIDATION_REQUEST | curl -s -X POST --data-binary @- http://localhost:11323/internal/vcr/v2/verifier/vc -H "Content-Type:application/json")
+if [[ $( echo $RESPONSE | jq -r .validity ) ==  "false" ]]; then
+  echo "  VC considered revoked by node-A"
+else
+  echo "  FAILED: Credential not revoked on node-A" 1>&2
+  echo $RESPONSE
+  exitWithDockerLogs 1
+fi
+
+echo "------------------------------------"
+echo "Stopping Docker containers..."
+echo "------------------------------------"
+docker compose stop
