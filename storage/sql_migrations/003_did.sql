51fcd8749dfe4a7ab12d5fae4bfd7f0e37496ad2
Wout Slakhorst <wout.slakhorst@nedap.com>
2024-03-08 09:14:49 +0100

    set primary key for did service and vm to id

--- storage/sql_migrations/003_did.sql
+++ storage/sql_migrations/003_did.sql
@@ -17,7 +17,7 @@ create table did_verificationmethod
     -- data is a JSON object containing the verification method data, e.g. the public key.
     -- When producing the verificationMethod, data is used as JSON base object and the id and type are added.
     data text         not null,
-    primary key (did, id),
+    primary key (id),
     foreign key (did) references did (did) on delete cascade
 );
 
@@ -31,7 +31,7 @@ create table did_service
     -- data is a JSON object containing the service data, e.g. the serviceEndpoint.
     -- When producing the service, data is used as JSON base object and the id and type are added.
     data text         not null,
-    primary key (did, id),
+    primary key (id),
     foreign key (did) references did (did) on delete cascade
 );
 
