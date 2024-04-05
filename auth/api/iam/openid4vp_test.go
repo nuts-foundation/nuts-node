4dc69d30278f5fb991b665652c5c9a372c4b052e
Rein Krul <info@reinkrul.nl>
2024-04-05 14:20:21 +0200

    IAM: Remove OpenID4VP PoC code

--- auth/api/iam/openid4vp_test.go
+++ auth/api/iam/openid4vp_test.go
@@ -24,23 +24,16 @@ import (
 	"encoding/json"
 	"github.com/lestrrat-go/jwx/v2/jwt"
 	"net/http"
-	"net/url"
 	"strings"
 	"testing"
 
-	ssi "github.com/nuts-foundation/go-did"
 	"github.com/nuts-foundation/go-did/did"
 	"github.com/nuts-foundation/go-did/vc"
-	"github.com/nuts-foundation/nuts-node/auth"
 	"github.com/nuts-foundation/nuts-node/auth/oauth"
-	"github.com/nuts-foundation/nuts-node/policy"
 	"github.com/nuts-foundation/nuts-node/storage"
 	"github.com/nuts-foundation/nuts-node/test"
-	"github.com/nuts-foundation/nuts-node/vcr"
-	"github.com/nuts-foundation/nuts-node/vcr/credential"
 	"github.com/nuts-foundation/nuts-node/vcr/holder"
 	"github.com/nuts-foundation/nuts-node/vcr/pe"
-	"github.com/nuts-foundation/nuts-node/vdr"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
@@ -694,99 +687,6 @@ func TestWrapper_sendAndHandleDirectPostError(t *testing.T) {
 
 		require.Error(t, err)
 		require.Equal(t, "server_error - something went wrong", err.Error())
-	})
-}
-
-func TestWrapper_sendPresentationRequest(t *testing.T) {
-	instance := New(nil, nil, nil, nil, nil)
-
-	redirectURI, _ := url.Parse("https://example.com/redirect")
-	verifierID, _ := url.Parse("https://example.com/verifier")
-	walletID, _ := url.Parse("https://example.com/wallet")
-
-	httpResponse := &stubResponseWriter{}
-
-	err := instance.sendPresentationRequest(context.Background(), httpResponse, "test-scope", *redirectURI, *verifierID, *walletID)
-
-	require.NoError(t, err)
-	require.Equal(t, http.StatusFound, httpResponse.statusCode)
-	location := httpResponse.headers.Get("Location")
-	require.NotEmpty(t, location)
-	locationURL, err := url.Parse(location)
-	require.NoError(t, err)
-	assert.Equal(t, "https", locationURL.Scheme)
-	assert.Equal(t, "example.com", locationURL.Host)
-	assert.Equal(t, "/wallet/authorize", locationURL.Path)
-	assert.Equal(t, "test-scope", locationURL.Query().Get("scope"))
-	assert.Equal(t, "vp_token id_token", locationURL.Query().Get("response_type"))
-	assert.Equal(t, "direct_post", locationURL.Query().Get("response_mode"))
-	assert.Equal(t, "https://example.com/verifier/.well-known/openid-wallet-metadata/metadata.xml", locationURL.Query().Get("client_metadata_uri"))
-}
-
-func TestWrapper_handlePresentationRequest(t *testing.T) {
-	credentialID, _ := ssi.ParseURI("did:web:example.com:issuer#6AF53584-3337-4766-8C8D-0BFD54F6E527")
-	walletCredentials := []vc.VerifiableCredential{
-		{
-			Context: []ssi.URI{
-				vc.VCContextV1URI(),
-				credential.NutsV1ContextURI,
-			},
-			ID:     credentialID,
-			Issuer: issuerDID.URI(),
-			Type:   []ssi.URI{vc.VerifiableCredentialTypeV1URI(), *credential.NutsOrganizationCredentialTypeURI},
-			CredentialSubject: []interface{}{
-				map[string]interface{}{
-					"id": holderDID.URI(),
-					"organization": map[string]interface{}{
-						"name": "Test Organization",
-						"city": "Test City",
-					},
-				},
-			},
-		},
-	}
-	t.Run("with scope", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		mockVDR := vdr.NewMockVDR(ctrl)
-		mockVCR := vcr.NewMockVCR(ctrl)
-		mockWallet := holder.NewMockWallet(ctrl)
-		mockPolicy := policy.NewMockPDPBackend(ctrl)
-		mockVCR.EXPECT().Wallet().Return(mockWallet)
-		mockAuth := auth.NewMockAuthenticationServices(ctrl)
-		mockWallet.EXPECT().List(gomock.Any(), holderDID).Return(walletCredentials, nil)
-		mockVDR.EXPECT().IsOwner(gomock.Any(), holderDID).Return(true, nil)
-		instance := New(mockAuth, mockVCR, mockVDR, storage.NewTestStorageEngine(t), mockPolicy)
-
-		params := map[string]interface{}{
-			"scope":                   "example-scope",
-			"response_type":           "code",
-			"response_mode":           "direct_post",
-			"client_metadata_uri":     "https://example.com/client_metadata.xml",
-			"presentation_definition": `{"id":"1","input_descriptors":[]}`,
-		}
-
-		response, err := instance.handlePresentationRequest(context.Background(), params, createSession(params, holderDID))
-
-		require.NoError(t, err)
-		httpResponse := &stubResponseWriter{}
-		_ = response.VisitHandleAuthorizeRequestResponse(httpResponse)
-		require.Equal(t, http.StatusOK, httpResponse.statusCode)
-		assert.Contains(t, httpResponse.body.String(), "</html>")
-	})
-	t.Run("invalid response_mode", func(t *testing.T) {
-		instance := New(nil, nil, nil, nil, nil)
-		params := map[string]interface{}{
-			"scope":                   "example-scope",
-			"response_type":           "code",
-			"response_mode":           "invalid",
-			"client_metadata_uri":     "https://example.com/client_metadata.xml",
-			"presentation_definition": "{}",
-		}
-
-		response, err := instance.handlePresentationRequest(context.Background(), params, createSession(params, holderDID))
-
-		requireOAuthError(t, err, oauth.InvalidRequest, "response_mode must be direct_post")
-		assert.Nil(t, response)
 	})
 }
 
