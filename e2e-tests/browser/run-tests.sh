664ef47983df5daa25408a5ed05caf688e7407ad
Rein Krul <info@reinkrul.nl>
2024-04-30 08:35:05 +0200

    enable e2e test

--- e2e-tests/browser/run-tests.sh
+++ e2e-tests/browser/run-tests.sh
@@ -2,6 +2,14 @@
 
 set -e # make script fail if any of the tests returns a non-zero exit code
 
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!! Running test: OpenID4VP Employee Credential !!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+pushd openid4vp_employeecredential
+./run-test.sh
+popd
+
+
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!! Running test: RFC019 Employee Credential !!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
