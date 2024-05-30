f413c5591d0cc51ad386f1e84ecbbd39c773a961
Rein Krul <info@reinkrul.nl>
2024-05-30 14:13:44 +0200

    $text_type

--- storage/sql_migrations/008_session_store.sql
+++ storage/sql_migrations/008_session_store.sql
@@ -1,10 +1,11 @@
+-- +goose ENVSUB ON
 -- +goose Up
 create table session_store
 (
     store   varchar(100) not null,
     expires integer      not null,
     key     varchar(100) not null,
-    value   text         not null,
+    value   $TEXT_TYPE   not null,
     PRIMARY KEY (store, key)
 );
 create index idx_session_store_expires on session_store (expires);
