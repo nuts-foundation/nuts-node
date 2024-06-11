c414c36bea71773828c99bf5c07ddffa93c752fc
Rein Krul <info@reinkrul.nl>
2024-06-11 18:44:51 +0200

    wip

--- storage/sql_migrations/008_tenant.sql
+++ storage/sql_migrations/008_tenant.sql
@@ -3,43 +3,50 @@
 -- this table is used to store tenants of the local node. Tenants hold 1 or more DIDs.
 create table tenant
 (
-    id      varchar(40) primary key,
-    created timestamp not null default current_timestamp
+    id         varchar(40) primary key,
+    -- created_at is the seconds since Unix Epoch when this tenant was created
+    created_at integer not null
 );
 
 create table tenant_did (
-    tenant_id varchar(40) not null,
-    did       varchar(370) not null,
+    tenant_id   varchar(40) not null,
+    did         varchar(370) not null,
     primary key (tenant_id, did),
-    foreign key (tenant_id) references tenant (id) on delete cascade,
+    foreign key (tenant_id) references tenant (id) on delete cascade
 );
+
 -- this table is used to store the verification methods for locally managed DIDs
 create table tenant_verificationmethod
 (
-    id  varchar(40) not null,
     tenant_id varchar(40) not null,
+    -- method_id is unique within the tenant. It will typically appear as verification method ID fragment in the DID documents.
+    method_id varchar(80) not null,
     -- data is a JSON object containing the verification method data, e.g. the public key.
     -- When producing the verificationMethod, data is used as JSON base object and the id and type are added.
     data $TEXT_TYPE not null,
-    primary key (id),
-    foreign key (did) references did (did) on delete cascade
+    primary key (tenant_id, method_id),
+    foreign key (tenant_id) references tenant (id) on delete cascade
 );
+create index idx_tenant_verificationmethod_tenant on tenant_verificationmethod (tenant_id);
 
--- this table is used to store the services for locally managed DIDs
-create table did_service
+-- this table is used to store the services for tenants
+create table tenant_service
 (
-    -- id is the unique id of the service as it appears in the DID document.
-    id  varchar(415) not null,
-    -- did references the containing DID
-    did varchar(370) not null,
+    tenant_id varchar(40) not null,
+    -- service_id is unique within the tenant. It will typically appear as service ID fragment in the DID documents.
+    service_id  varchar(80) not null,
     -- data is a JSON object containing the service data, e.g. the serviceEndpoint.
     -- When producing the service, data is used as JSON base object and the id and type are added.
     data $TEXT_TYPE not null,
-    primary key (id),
-    foreign key (did) references did (did) on delete cascade
+    primary key (tenant_id, service_id),
+    foreign key (tenant_id) references tenant (id) on delete cascade
 );
+create index idx_tenant_service_tenant on tenant_service (tenant_id);
 
 -- +goose Down
-drop table did;
-drop table did_verificationmethod;
-drop table did_service;
+drop table tenant;
+drop table tenant_did;
+drop index idx_tenant_verificationmethod_tenant;
+drop table tenant_verificationmethod;
+drop index idx_tenant_service_tenant;
+drop table tenant_service;
--- tenant/diddocument/store.go
+++ tenant/diddocument/store.go
@@ -1,17 +1,54 @@
 package diddocument
 
 import (
+	"encoding/json"
+	"errors"
+	ssi "github.com/nuts-foundation/go-did"
 	"github.com/nuts-foundation/go-did/did"
 	"github.com/nuts-foundation/nuts-node/vdr/resolver"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
-type SQLStore struct {
+type sqlTenant struct {
+	ID                  string                  `gorm:"primaryKey"`
+	VerificationMethods []sqlVerificationMethod `gorm:"foreignKey:TenantID;references:ID"`
+	Services            []sqlService            `gorm:"foreignKey:TenantID;references:ID"`
 }
 
+var _ schema.Tabler = (*sqlVerificationMethod)(nil)
+
+type sqlVerificationMethod struct {
+	TenantID string `gorm:"primaryKey"`
+	MethodID string `gorm:"primaryKey"`
+	Data     []byte
+}
+
+func (v sqlVerificationMethod) TableName() string {
+	return "tenant_verificationmethod"
+}
+
+var _ schema.Tabler = (*sqlService)(nil)
+
+type sqlService struct {
+	TenantID  string `gorm:"primaryKey"`
+	ServiceID string `gorm:"primaryKey"`
+	Data      []byte
+}
+
+func (v sqlService) TableName() string {
+	return "tenant_service"
+}
+
+type SQLStore struct {
+	DB *gorm.DB
+}
+
+// Get retrieves the DID document Verification Methods and Services for the given tenant.
+// It constructs the IDs based on the given DID and the object's stored ID.
 func (s *SQLStore) Get(tenantID string, id did.DID) ([]did.VerificationMethod, []did.Service, error) {
-	var record sqlDID
-	err := s.db.Model(&sqlDID{}).Where("did = ?", id.String()).
+	var record sqlTenant
+	err := s.DB.Model(&sqlTenant{}).Where("tenant_id = ?", tenantID).
 		Preload("VerificationMethods").
 		Preload("Services").
 		First(&record).Error
@@ -29,12 +66,10 @@ func (s *SQLStore) Get(tenantID string, id did.DID) ([]did.VerificationMethod, [
 			return nil, nil, err
 		}
 		verificationMethods = append(verificationMethods, method)
-		vmID, err := did.ParseDIDURL(curr.ID)
-		if err != nil {
-			// weird
-			return nil, nil, err
-		}
-		method.ID = *vmID
+		var vmID did.DIDURL
+		vmID.DID = id
+		vmID.Fragment = curr.MethodID
+		method.ID = vmID
 	}
 
 	var services []did.Service
@@ -43,8 +78,60 @@ func (s *SQLStore) Get(tenantID string, id did.DID) ([]did.VerificationMethod, [
 		if err := json.Unmarshal(curr.Data, &service); err != nil {
 			return nil, nil, err
 		}
+		var serviceID did.DIDURL
+		serviceID.DID = id
+		serviceID.Fragment = curr.ServiceID
+		service.ID = serviceID.URI()
 		services = append(services, service)
 	}
 
 	return verificationMethods, services, nil
+}
+
+// createService creates a new service in the DID document identified by subjectDID.
+// It does not validate the service.
+func (s *sqlStore) createService(subjectDID did.DID, service did.Service) error {
+	data, _ := json.Marshal(service)
+	record := &sqlService{
+		ID:   service.ID.String(),
+		Did:  subjectDID.String(),
+		Data: data,
+	}
+	err := s.db.Create(record).Error
+	if errors.Is(err, gorm.ErrDuplicatedKey) {
+		return errDuplicateService
+	}
+	if errors.Is(err, gorm.ErrForeignKeyViolated) {
+		return errServiceDIDNotFound
+	}
+	return err
+}
+
+func (s *sqlStore) updateService(subjectDID did.DID, id ssi.URI, service did.Service) error {
+	data, _ := json.Marshal(service)
+	record := &sqlService{
+		ID:   service.ID.String(),
+		Did:  subjectDID.String(),
+		Data: data,
+	}
+	result := s.db.Model(&sqlService{}).Where("did = ? AND id = ?", subjectDID.String(), id.String()).Updates(record)
+	err := result.Error
+	if errors.Is(err, gorm.ErrDuplicatedKey) {
+		return errDuplicateService
+	}
+	if result.RowsAffected == 0 {
+		return errServiceNotFound
+	}
+	return nil
+}
+
+func (s *sqlStore) deleteService(subjectDID did.DID, id ssi.URI) error {
+	result := s.db.Model(&sqlService{}).Where("did = ? AND id = ?", subjectDID.String(), id.String()).Delete(&sqlService{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errServiceNotFound
+	}
+	return nil
 }
