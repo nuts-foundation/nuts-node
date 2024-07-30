b97da33d40bce5554a4ca71919ebfd6af358feec
Wout Slakhorst <wout.slakhorst@nedap.com>
2024-07-30 13:10:05 +0200

    remove legacy API auth (#3280)

--- http/cmd/cmd.go
+++ http/cmd/cmd.go
@@ -19,17 +19,9 @@
 package cmd
 
 import (
-	"crypto"
 	"fmt"
-	"github.com/lestrrat-go/jwx/v2/jwt"
-	"github.com/nuts-foundation/nuts-node/audit"
-	cryptoCmd "github.com/nuts-foundation/nuts-node/crypto/cmd"
 	"github.com/nuts-foundation/nuts-node/http"
-	"github.com/nuts-foundation/nuts-node/network"
-	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"strconv"
-	"time"
 )
 
 // FlagSet defines the set of flags that sets the engine configuration
@@ -46,68 +38,4 @@ func FlagSet() *pflag.FlagSet {
 	flags.Int("http.cache.maxbytes", defs.ResponseCacheSize, "HTTP client maximum size of the response cache in bytes. If 0, the HTTP client does not cache responses.")
 
 	return flags
-}
-
-// ServerCmd contains sub-commands for the HTTP engine
-func ServerCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "http",
-		Short: "http commands",
-	}
-	cmd.AddCommand(createTokenCommand())
-	return cmd
-}
-
-func createTokenCommand() *cobra.Command {
-	return &cobra.Command{
-		Use:   "gen-token [user name] [days valid]",
-		Short: "Generates an access token for administrative operations.",
-		Args:  cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := audit.Context(cmd.Context(), "app-cli", network.ModuleName, cmd.Name())
-
-			daysValid, err := strconv.Atoi(args[1])
-			if err != nil {
-				return err
-			}
-			user := args[0]
-
-			cmd.Println(fmt.Sprintf("Generating API token for user %s, valid for %d days...", user, daysValid))
-
-			instance, err := cryptoCmd.LoadCryptoModule(cmd)
-			if err != nil {
-				return err
-			}
-
-			exists, err := instance.Exists(cmd.Context(), http.AdminTokenSigningKID)
-			if err != nil {
-				return err
-			}
-			if !exists {
-				cmd.Println("Token signing key not found, generating new key...")
-				_, err := instance.New(ctx, func(key crypto.PublicKey) (string, error) {
-					return http.AdminTokenSigningKID, nil
-				})
-				if err != nil {
-					return err
-				}
-			}
-			token, err := instance.SignJWT(ctx, map[string]interface{}{
-				jwt.SubjectKey:    user,
-				jwt.ExpirationKey: time.Now().AddDate(0, 0, daysValid),
-			}, nil, http.AdminTokenSigningKID)
-			if err != nil {
-				return err
-			}
-			cmd.Println()
-			cmd.Println("Token:")
-			cmd.Println()
-			cmd.Println(token)
-			cmd.Println()
-			cmd.Println("You can provide it manually when executing CLI commands (using --token), " +
-				"or save it in a file and use --token-file to have the CLI read it.")
-
-			return nil
-		},
-	}
 }
