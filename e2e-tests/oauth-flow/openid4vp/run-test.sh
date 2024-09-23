5b6050636aee16e7d93fac19deaf863d7cf1d2ed
Wout Slakhorst <wout.slakhorst@nedap.com>
2024-09-23 10:31:07 +0200

    PR feeback

--- e2e-tests/oauth-flow/openid4vp/run-test.sh
+++ e2e-tests/oauth-flow/openid4vp/run-test.sh
@@ -2,4 +2,5 @@
 set -e
 ./do-test.sh didweb-user
 # skip test, root did:web is not supported and test is broken
+# see https://github.com/nuts-foundation/nuts-node/issues/3299
 # ./do-test.sh didweb-root
