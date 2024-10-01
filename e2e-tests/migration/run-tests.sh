377981e9d70cba84ea046b3a55c5b6af5f6bf834
Gerard Snaauw <gerard.snaauw@nedap.com>
2024-10-01 13:07:00 +0200

    migration: did:nuts to SQL

---
+++ e2e-tests/migration/run-tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e # make script fail if any of the tests returns a non-zero exit code
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!! Running test: Migrations v6    !!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+./run-test.sh
