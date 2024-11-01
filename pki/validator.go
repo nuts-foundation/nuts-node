3de0e38e5ea6073cd70c24219c33386a66a6fbe0
Gerard Snaauw <gerard.snaauw@nedap.com>
2024-11-01 10:43:26 +0100

    PKI Valdiator always fails on unknown CAs

--- pki/validator.go
+++ pki/validator.go
@@ -143,8 +143,8 @@ func (v *validator) validate(chain []*x509.Certificate, softfail bool) error {
 		// check in reverse order to prevent CRL expiration errors due to revoked CAs no longer issuing CRLs
 		if err = v.validateCert(cert); err != nil {
 			errOut := fmt.Errorf("%w: subject=%s, S/N=%s, issuer=%s", err, cert.Subject.String(), cert.SerialNumber.String(), cert.Issuer.String())
-			if softfail && !(errors.Is(err, ErrCertRevoked) || errors.Is(err, ErrCertBanned)) {
-				// Accept the certificate even if it cannot be properly validated
+			if softfail && (errors.Is(err, ErrCRLExpired) || errors.Is(err, ErrCRLMissing) || errors.Is(err, ErrDenylistMissing)) {
+				// Accept the certificate even if it cannot be properly validated against the CRL or denylist
 				logger().WithError(errOut).Error("Certificate CRL check softfail bypass. Might be unsafe, find cause of failure!")
 				continue
 			}
--- pki/validator_test.go
+++ pki/validator_test.go
@@ -136,7 +136,7 @@ func TestValidator_Validate(t *testing.T) {
 	})
 	t.Run("unknown issuer", func(t *testing.T) {
 		val := &validator{}
-		testSoftHard(t, val, validCertA, nil, ErrCertUntrusted)
+		testSoftHard(t, val, validCertA, ErrCertUntrusted, ErrCertUntrusted)
 	})
 	t.Run("missing crl", func(t *testing.T) {
 		testSoftHard(t, val, validCertBWithRevokedCA, nil, ErrCRLMissing)
