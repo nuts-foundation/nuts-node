aeedc7593677470b3091d04d4edec55fdefdcf48
Rein Krul <info@reinkrul.nl>
2025-02-11 11:36:31 +0100

    PR feedback

--- discovery/cmd/cmd.go
+++ discovery/cmd/cmd.go
@@ -38,7 +38,7 @@ func FlagSet() *pflag.FlagSet {
 			"refreshing Verifiable Presentations of local DIDs and loading changes, updating the local copy. "+
 			"It only will actually refresh registrations of local DIDs that about to expire (less than 1/4th of their lifetime left). "+
 			"Specified as Golang duration (e.g. 1m, 1h30m).")
-	flagSet.Duration("discovery.client.refresh_interval", 0,
-		"Deprecated, use refresh_interval.")
+	flagSet.Duration("discovery.client.refresh_interval", 0, "Deprecated, use refresh_interval instead.")
+	_ = flagSet.MarkDeprecated("discovery.client.refresh_interval", "Use refreshinterval instead.")
 	return flagSet
 }
