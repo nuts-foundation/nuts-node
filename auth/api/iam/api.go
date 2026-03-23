1114fcc61b8def6f9d7dd84e2a46c1981602c964
Rein Krul <info@reinkrul.nl>
2026-03-23 13:09:13 +0100

    Fix passing Dezi attestation as extra credential

--- auth/api/iam/api.go
+++ auth/api/iam/api.go
@@ -753,18 +753,25 @@ func (r Wrapper) RequestServiceAccessToken(ctx context.Context, request RequestS
 		credentials = *request.Body.Credentials
 	}
 
+	idTokenCredentialIdx := -1
 	if request.Body.IdToken != nil {
 		idTokenCredential, err := credential.CreateDeziUserCredential(*request.Body.IdToken)
 		if err != nil {
 			return nil, core.InvalidInputError("failed to create id_token credential: %w", err)
 		}
 		credentials = append(credentials, *idTokenCredential)
+		idTokenCredentialIdx = len(credentials) - 1
 	}
 
 	// assert that self-asserted credentials do not contain an issuer or credentialSubject.id. These values must be set
 	// by the nuts-node to build the correct wallet for a DID. See https://github.com/nuts-foundation/nuts-node/issues/3696
 	// As a side effect it is no longer possible to pass signed credentials to this API.
-	for _, cred := range credentials {
+	for i, cred := range credentials {
+		// But not for id_token credentials, these are externally signed, meaning they have an issuer
+		if i == idTokenCredentialIdx {
+			continue
+		}
+
 		var credentialSubject []map[string]interface{}
 		if err := cred.UnmarshalCredentialSubject(&credentialSubject); err != nil {
 			// extremely unlikely
